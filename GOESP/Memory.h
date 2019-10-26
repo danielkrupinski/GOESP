cc775870d6f755138fe703a78a1b61d1f458732f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-26 09:30:51 +0200

    Add Memory::findPattern() method

--- GOESP/Memory.h
+++ GOESP/Memory.h
@@ -1,5 +1,35 @@
 #pragma once
 
-class Memory {
+#include <cstdint>
+#include <Windows.h>
+#include <Psapi.h>
 
+class Memory {
+private:
+    template <typename T = uintptr_t>
+    static auto findPattern(const wchar_t* module, const char* pattern, size_t offset = 0) noexcept
+    {
+        if (MODULEINFO moduleInfo; GetModuleInformation(GetCurrentProcess(), GetModuleHandleW(module), &moduleInfo, sizeof(moduleInfo))) {
+            auto start{ static_cast<const char*>(moduleInfo.lpBaseOfDll) };
+            auto end{ start + moduleInfo.SizeOfImage };
+
+            auto first{ start };
+            auto second{ pattern };
+
+            while (first < end && *second) {
+                if (*first == *second || *second == '?') {
+                    first++;
+                    second++;
+                } else {
+                    first = ++start;
+                    second = pattern;
+                }
+            }
+
+            if (!*second)
+                return reinterpret_cast<T>(const_cast<char*>(start) + offset);
+        }
+        MessageBoxA(NULL, (std::ostringstream{ } << "Failed to find pattern in " << module << '!').str().c_str(), "Osiris", MB_OK | MB_ICONERROR);
+        std::exit(EXIT_FAILURE);
+    }
 };
