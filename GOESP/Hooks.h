6b9333cdb97a9b93b6f02e530b75cd66c475c754
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-26 10:28:41 +0200

    Create Hooks class

---
+++ GOESP/Hooks.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <d3d9.h>
+#include <type_traits>
+
+class Hooks {
+public:
+    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
+    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
+};
--- GOESP/dllmain.cpp
+++ GOESP/dllmain.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
