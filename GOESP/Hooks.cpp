f1ced7633dc53cf765e32e5f415a06c9683289db
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-26 11:28:23 +0200

    Add Hook struct

--- GOESP/Hooks.cpp
+++ GOESP/Hooks.cpp
@@ -5,12 +5,22 @@
 
 static HRESULT __stdcall present(IDirect3DDevice9* device, const RECT* src, const RECT* dest, HWND windowOverride, const RGNDATA* dirtyRegion) noexcept
 {
-    return hooks.originalPresent(device, src, dest, windowOverride, dirtyRegion);
+    hooks.present.hookCalled = true;
+
+    auto result{ hooks.present.original(device, src, dest, windowOverride, dirtyRegion) };
+
+    hooks.present.hookCalled = false;
+    return result;
 }
 
 static HRESULT __stdcall reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* params) noexcept
 {
-    return hooks.originalReset(device, params);
+    hooks.reset.hookCalled = true;
+    
+    auto result{ hooks.reset.original(device, params) };
+
+    hooks.reset.hookCalled = false;
+    return result;
 }
 
 Hooks::Hooks() noexcept
@@ -18,9 +28,9 @@ Hooks::Hooks() noexcept
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 
-    originalPresent = **reinterpret_cast<decltype(originalPresent)**>(memory.present);
-    **reinterpret_cast<void***>(memory.present) = reinterpret_cast<void*>(present);
+    present.original = **reinterpret_cast<decltype(present.original)**>(memory.present);
+    **reinterpret_cast<void***>(memory.present) = reinterpret_cast<void*>(::present);
 
-    originalReset = **reinterpret_cast<decltype(originalReset)**>(memory.reset);
-    **reinterpret_cast<void***>(memory.reset) = reinterpret_cast<void*>(reset);
+    reset.original = **reinterpret_cast<decltype(reset.original)**>(memory.reset);
+    **reinterpret_cast<void***>(memory.reset) = reinterpret_cast<void*>(::reset);
 }
--- GOESP/Hooks.h
+++ GOESP/Hooks.h
@@ -3,12 +3,18 @@
 #include <d3d9.h>
 #include <type_traits>
 
+template <typename T>
+struct Hook {
+    T* original;
+    bool hookCalled{ false };
+};
+
 class Hooks {
 public:
     Hooks() noexcept;
 
-    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
-    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
+    Hook<HRESULT __stdcall(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> present;
+    Hook<HRESULT __stdcall(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> reset;
 };
 
 extern Hooks hooks;
