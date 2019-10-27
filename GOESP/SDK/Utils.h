3972eac178509fc43d6f34c5a76368abaedc2851
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-27 19:25:51 +0100

    Create Utils.h

---
+++ GOESP/SDK/Utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template<typename T, typename ...Args>
+constexpr auto callVirtualMethod(void* classBase, int index, Args... args) noexcept
+{
+    return ((*reinterpret_cast<T(__thiscall***)(void*, Args...)>(classBase))[index])(classBase, args...);
+}
