1486e9e2ab74218ea0e3b79c9973d28046c93df2
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-27 19:44:40 +0100

    Add InputSystem::enableInput() method

--- GOESP/SDK/InputSystem.h
+++ GOESP/SDK/InputSystem.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Utils.h"
+
+class InputSystem {
+public:
+    constexpr void enableInput(bool enable) noexcept
+    {
+        callVirtualMethod<void, bool>(this, 11, enable);
+    }
+};
