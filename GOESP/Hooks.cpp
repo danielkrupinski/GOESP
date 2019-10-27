f2a2a46946a071508c1fffec5ade582f7c7b816f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-27 09:31:14 +0100

    Draw imgui demo window

--- GOESP/Hooks.cpp
+++ GOESP/Hooks.cpp
@@ -1,11 +1,34 @@
 #include <intrin.h>
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
+
 #include "Hooks.h"
 #include "Memory.h"
 
 static HRESULT __stdcall present(IDirect3DDevice9* device, const RECT* src, const RECT* dest, HWND windowOverride, const RGNDATA* dirtyRegion) noexcept
 {
     hooks.present.hookCalled = true;
+
+    static bool imguiInit{ ImGui_ImplDX9_Init(device) };
+
+    IDirect3DVertexDeclaration9* vertexDeclaration;
+    device->GetVertexDeclaration(&vertexDeclaration);
+
+    ImGui_ImplDX9_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+
+    ImGui::EndFrame();
+
+    ImGui::Render();
+    ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+    device->SetVertexDeclaration(vertexDeclaration);
+    vertexDeclaration->Release();
 
     auto result{ hooks.present.original(device, src, dest, windowOverride, dirtyRegion) };
 
@@ -27,6 +50,9 @@ Hooks::Hooks() noexcept
 {
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+
+    ImGui::CreateContext();
+    ImGui_ImplWin32_Init(FindWindowW(L"Valve001", NULL));
 
     present.original = **reinterpret_cast<decltype(present.original)**>(memory.present);
     **reinterpret_cast<decltype(::present)***>(memory.present) = ::present;
