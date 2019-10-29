5536d70d86b88d29a629f2488c24afd7fe787181
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 20:34:26 +0100

    Add ClientClass struct

--- GOESP/SDK/ClientClass.h
+++ GOESP/SDK/ClientClass.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "ClassId.h"
+
+#include <type_traits>
+
+class Entity;
+struct RecvTable;
+
+struct ClientClass {
+    std::add_pointer_t<Entity* __cdecl(int, int)> createFunction;
+    void* createEventFunction;
+    char* networkName;
+    RecvTable* recvTable;
+    ClientClass* next;
+    ClassId classId;
+};
