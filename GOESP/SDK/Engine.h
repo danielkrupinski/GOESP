aa8e7c584ed3d9cefa5ee7d541953813ac297d82
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 21:16:53 +0100

    Add Engine::getScreenSize() method

--- GOESP/SDK/Engine.h
+++ GOESP/SDK/Engine.h
@@ -2,8 +2,17 @@
 
 #include "Utils.h"
 
+#include <tuple>
+
 class Engine {
 public:
+    constexpr auto getScreenSize() noexcept
+    {
+        int w = 0, h = 0;
+        callVirtualMethod<void, int&, int&>(this, 5, w, h);
+        return std::make_pair(w, h);
+    }
+
     constexpr auto getLocalPlayer() noexcept
     {
         return callVirtualMethod<int>(this, 12);
