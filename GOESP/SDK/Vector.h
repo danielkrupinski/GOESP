820cbf170cc3da7bcbb7742efc86f4dd5cce6626
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 20:06:10 +0100

    Add Vector struct

--- GOESP/SDK/Vector.h
+++ GOESP/SDK/Vector.h
@@ -1,1 +1,74 @@
 #pragma once
+
+#include <cmath>
+
+struct Vector {
+    constexpr operator bool() const noexcept
+    {
+        return x || y || z;
+    }
+
+    constexpr Vector& operator=(float array[3]) noexcept
+    {
+        x = array[0];
+        y = array[1];
+        z = array[2];
+        return *this;
+    }
+
+    constexpr Vector& operator+=(const Vector& v) noexcept
+    {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+
+    constexpr Vector& operator-=(const Vector& v) noexcept
+    {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+
+    constexpr auto operator-(const Vector& v) const noexcept
+    {
+        return Vector{ x - v.x, y - v.y, z - v.z };
+    }
+
+    constexpr auto operator+(const Vector& v) const noexcept
+    {
+        return Vector{ x + v.x, y + v.y, z + v.z };
+    }
+
+    constexpr Vector& operator/=(float div) noexcept
+    {
+        x /= div;
+        y /= div;
+        z /= div;
+        return *this;
+    }
+
+    constexpr auto operator*(float mul) const noexcept
+    {
+        return Vector{ x * mul, y * mul, z * mul };
+    }
+
+    auto length() const noexcept
+    {
+        return std::sqrtf(x * x + y * y + z * z);
+    }
+
+    constexpr auto squareLength() const noexcept
+    {
+        return x * x + y * y + z * z;
+    }
+
+    constexpr auto dotProduct(const Vector& v) const noexcept
+    {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    float x, y, z;
+};
