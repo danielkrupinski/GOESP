9666b1a56c1da5b2a398ece5ca7935955138d1bb
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 20:30:41 +0100

    Add Client class

--- GOESP/SDK/Client.h
+++ GOESP/SDK/Client.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Utils.h"
+
+struct ClientClass;
+
+class Client {
+public:
+    constexpr auto getAllClasses() noexcept
+    {
+        return callVirtualMethod<ClientClass*>(this, 8);
+    }
+};
