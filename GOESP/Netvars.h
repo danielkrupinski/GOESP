e255954c6f6c1ec4f801851fc80e37ae28df2c2c
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 20:37:35 +0100

    Add Netvars::traverseTable() method

--- GOESP/Netvars.h
+++ GOESP/Netvars.h
@@ -1,5 +1,28 @@
 #pragma once
 
-class Netvars {
+#include <cstdint>
+#include <unordered_map>
 
+#include "Interfaces.h"
+#include "SDK/Client.h"
+#include "SDK/ClientClass.h"
+
+struct RecvTable;
+
+class Netvars {
+public:
+    auto operator[](const uint32_t hash) noexcept
+    {
+        if (!initialized) {
+            for (auto clientClass = interfaces.client->getAllClasses(); clientClass; clientClass = clientClass->next)
+                traverseTable(clientClass->networkName, clientClass->recvTable);
+
+            initialized = true;
+        }
+        return offsets[hash];
+    }
+private:
+    bool initialized = false;
+    void traverseTable(const char*, RecvTable*, const size_t = 0) noexcept;
+    std::unordered_map<uint32_t, uint16_t> offsets;
 };
