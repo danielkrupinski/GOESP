97a0d73796d0f3e0f5881ef185c077af0f6b707e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 19:23:31 +0100

    Add Engine class

--- GOESP/SDK/Engine.h
+++ GOESP/SDK/Engine.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "Utils.h"
+
+class Engine {
+public:
+    constexpr auto getLocalPlayer() noexcept
+    {
+        return callVirtualMethod<int>(this, 12);
+    }
+
+    constexpr auto getMaxClients() noexcept
+    {
+        return callVirtualMethod<int>(this, 20);
+    }
+
+    constexpr auto isInGame() noexcept
+    {
+        return callVirtualMethod<bool>(this, 26);
+    }
+};
