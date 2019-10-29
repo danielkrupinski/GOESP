e7174048f18f22bf992ef105a5481aca0d8d75a1
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 20:51:26 +0100

    Add Entity::isDormant() method

--- GOESP/SDK/Entity.h
+++ GOESP/SDK/Entity.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include "../Netvars.h"
+#include "Utils.h"
 
 class Entity {
 public:
+    constexpr bool isDormant() noexcept
+    {
+        return callVirtualMethod<bool>(this + 8, 9);
+    }
+
     NETVAR(health, "CBasePlayer", "m_iHealth", int);
 };
