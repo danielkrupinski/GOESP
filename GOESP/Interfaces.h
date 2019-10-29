ddb02b25a99d204c1c0e8d392a500fde21f981b8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 21:08:03 +0100

    Capture VDebugOverlay004 interface

--- GOESP/Interfaces.h
+++ GOESP/Interfaces.h
@@ -5,6 +5,7 @@
 #include <Windows.h>
 
 class Client;
+class DebugOverlay;
 class Engine;
 class EntityList;
 class InputSystem;
@@ -12,6 +13,7 @@ class InputSystem;
 class Interfaces {
 public:
     Client* client = find<Client>(L"client_panorama", "VClient018");
+    DebugOverlay* debugOverlay = find<DebugOverlay>(L"engine", "VDebugOverlay004");
     Engine* engine = find<Engine>(L"engine", "VEngineClient014");
     EntityList* entityList = find<EntityList>(L"client_panorama", "VClientEntityList003");
     InputSystem* inputSystem = find<InputSystem>(L"inputsystem", "InputSystemVersion001");
