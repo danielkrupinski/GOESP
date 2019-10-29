d14151255b2b899a177ef75384a21f29f04e93d8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 18:28:41 +0100

    Add listbox for chosing esp category

--- GOESP/GUI.cpp
+++ GOESP/GUI.cpp
@@ -5,6 +5,7 @@
 
 #include "Hooks.h"
 
+#include <array>
 #include <ctime>
 #include <iomanip>
 #include <sstream>
@@ -35,12 +36,58 @@ void GUI::render() noexcept
 
     blockInput = ImGui::Begin(windowTitle.str().c_str());
 
+    static int currentCategory = 0;
+    static int currentItem = 0;
+
+    if (ImGui::ListBoxHeader("##list", { 125.0f, 300.0f })) {
+        static constexpr std::array categories{ "Allies", "Enemies" };
+
+        for (size_t i = 0; i < categories.size(); i++) {
+            if (ImGui::Selectable(categories[i], currentCategory == i && currentItem == 0)) {
+                currentCategory = i;
+                currentItem = 0;
+            }
+            ImGui::PushID(i);
+
+            constexpr auto getItems = [](int index) constexpr noexcept -> std::initializer_list<const char*> {
+                switch (index) {
+                case 0:
+                case 1:
+                    return { "Visible", "Occluded" };
+                case 2:
+                    // return { "AK-47", "Tec-9" };
+                case 3:
+                    // return { "Smoke", "HE Grenade", "Flashbang" };
+                default:
+                    return { };
+                }
+            };
+
+            ImGui::Indent();
+
+            auto items = getItems(i);
+            for (auto it = items.begin(); it != items.end(); it++) {
+                auto itemIndex = std::distance(items.begin(), it) + 1;
+
+                if (ImGui::Selectable(*it, currentCategory == i && currentItem == itemIndex)) {
+                    currentCategory = i;
+                    currentItem = itemIndex;
+                }
+            }
+
+            ImGui::Unindent();
+            ImGui::PopID();
+        }
+        ImGui::ListBoxFooter();
+    }
+
+    ImGui::Separator();
+
     ImGui::TextUnformatted("Build date: " __DATE__ " " __TIME__);
     ImGui::SameLine(0.0f, 30.0f);
 
     if (ImGui::Button("Unload"))
         hooks.restore();
 
-    ImGui::Separator();
     ImGui::End();
 }
