af3c97270f7945a521296171c20663f604a6ae3c
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 21:09:49 +0100

    Add Entity::getCollideable() method

--- GOESP/SDK/Entity.h
+++ GOESP/SDK/Entity.h
@@ -3,11 +3,25 @@
 #include "../Netvars.h"
 #include "Utils.h"
 
+struct Vector;
+
+class Collideable {
+public:
+    virtual void* pad() = 0;
+    virtual const Vector& obbMins() = 0;
+    virtual const Vector& obbMaxs() = 0;
+};
+
 class Entity {
 public:
     constexpr bool isDormant() noexcept
     {
         return callVirtualMethod<bool>(this + 8, 9);
+    }
+
+    constexpr auto getCollideable() noexcept
+    {
+        return callVirtualMethod<Collideable*>(this, 3);
     }
 
     constexpr bool isAlive() noexcept
