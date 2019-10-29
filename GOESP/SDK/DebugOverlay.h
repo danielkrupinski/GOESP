77cd9639d9e778d79cf52ef13e4c0f2d606eb4d4
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 21:05:38 +0100

    Add DebugOverlay::screenPosition() method

--- GOESP/SDK/DebugOverlay.h
+++ GOESP/SDK/DebugOverlay.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Utils.h"
+
+struct Vector;
+
+class DebugOverlay {
+public:
+    constexpr auto screenPosition(const Vector& point, Vector& screen) noexcept
+    {
+        return callVirtualMethod<bool, const Vector&, Vector&>(this, 13, point, screen);
+    }
+};
