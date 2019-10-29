77f555805c554183bb6a4e6b2c9c658382ba9a2a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-29 19:12:30 +0100

    Add EntityList class

--- GOESP/SDK/EntityList.h
+++ GOESP/SDK/EntityList.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "Utils.h"
+
+class Entity;
+
+class EntityList {
+public:
+    constexpr auto getEntity(int index) noexcept
+    {
+        return callVirtualMethod<Entity*, int>(this, 3, index);
+    }
+
+    constexpr auto getEntityFromHandle(int handle) noexcept
+    {
+        return callVirtualMethod<Entity*, int>(this, 4, handle);
+    }
+
+    constexpr auto getHighestEntityIndex() noexcept
+    {
+        return callVirtualMethod<int>(this, 6);
+    }
+};
