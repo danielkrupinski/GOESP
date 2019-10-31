e50d5cec259fab254d602f69115103bd3b716533
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-31 11:55:24 +0100

    Add packColor() function

--- GOESP/SDK/Utils.h
+++ GOESP/SDK/Utils.h
@@ -5,3 +5,8 @@ constexpr auto callVirtualMethod(void* classBase, int index, Args... args) noexc
 {
     return ((*reinterpret_cast<T(__thiscall***)(void*, Args...)>(classBase))[index])(classBase, args...);
 }
+
+constexpr unsigned packColor(float color[3]) noexcept
+{
+    return 0xFF000000 | unsigned(color[0] * 255) | (unsigned(color[1] * 255) << 8) | (unsigned(color[2] * 255) << 16);
+}
