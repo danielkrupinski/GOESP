556882eaf011a564bb578cba8a70bd86fc4372f5
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-31 11:56:01 +0100

    Add boundingBox() function

--- GOESP/ESP.cpp
+++ GOESP/ESP.cpp
@@ -2,10 +2,55 @@
 
 #include "imgui/imgui.h"
 
+#include "Config.h"
 #include "Interfaces.h"
+#include "SDK/DebugOverlay.h"
 #include "SDK/Engine.h"
 #include "SDK/Entity.h"
 #include "SDK/EntityList.h"
+#include "SDK/Utils.h"
+#include "SDK/Vector.h"
+
+struct BoundingBox {
+    float x0, y0;
+    float x1, y1;
+    Vector vertices[8];
+};
+
+static auto boundingBox(Entity* entity, BoundingBox& out) noexcept
+{
+    const auto [width, height] = interfaces.engine->getScreenSize();
+
+    out.x0 = static_cast<float>(width * 2);
+    out.y0 = static_cast<float>(height * 2);
+    out.x1 = -static_cast<float>(width * 2);
+    out.y1 = -static_cast<float>(height * 2);
+
+    const auto mins = entity->getCollideable()->obbMins();
+    const auto maxs = entity->getCollideable()->obbMaxs();
+
+    for (int i = 0; i < 8; i++) {
+        const Vector point{ i & 1 ? maxs.x : mins.x,
+                            i & 2 ? maxs.y : mins.y,
+                            i & 4 ? maxs.z : mins.z };
+
+        if (interfaces.debugOverlay->screenPosition(point.transform(entity->coordinateFrame()), out.vertices[i]))
+            return false;
+
+        if (out.x0 > out.vertices[i].x)
+            out.x0 = out.vertices[i].x;
+
+        if (out.y0 > out.vertices[i].y)
+            out.y0 = out.vertices[i].y;
+
+        if (out.x1 < out.vertices[i].x)
+            out.x1 = out.vertices[i].x;
+
+        if (out.y1 < out.vertices[i].y)
+            out.y1 = out.vertices[i].y;
+    }
+    return true;
+}
 
 void ESP::render(ImDrawList* drawList) noexcept
 {
@@ -20,10 +65,13 @@ void ESP::render(ImDrawList* drawList) noexcept
             if (!entity || entity == localPlayer || entity->isDormant() || !entity->isAlive())
                 continue;
 
+            if (BoundingBox bbox; boundingBox(entity, bbox)) {
+
+            }
 
         }
-        if (localPlayer->health() != 0)
-        drawList->AddCircle({ 300, 300 }, 15.0f, 0xFF00FF00);
     }
-    drawList->AddCircle({ 200, 200 }, 15.0f, 0xFFFF0000);
+    const auto [width, height] = interfaces.engine->getScreenSize();
+
+    drawList->AddCircle({ float(width / 2), float(height / 2) }, 15.0f, packColor(config.players[0].box.color));
 }
