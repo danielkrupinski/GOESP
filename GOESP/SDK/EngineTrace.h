004294c60a02d9f877e1ca8f0ac045d03c4a8df6
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-31 18:16:26 +0100

    Add EngineTrace class

--- GOESP/SDK/EngineTrace.h
+++ GOESP/SDK/EngineTrace.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#include "Utils.h"
+#include "Vector.h"
+
+#include <cstddef>
+
+struct Ray {
+    Ray(const Vector& src, const Vector& dest) : start(src), delta(dest - src) { isSwept = delta.x || delta.y || delta.z; }
+    Vector start{ };
+    float pad = 0.0f;
+    Vector delta{ };
+    std::byte pad2[40]{ };
+    bool isRay = true;
+    bool isSwept = false;
+};
+
+class Entity;
+
+struct TraceFilter {
+    TraceFilter(const Entity* entity) : skip{ entity } { }
+    virtual bool shouldHitEntity(Entity* entity, int) { return entity != skip; }
+    virtual int getTraceType() const { return 0; }
+    const void* skip;
+};
+
+struct Trace {
+    Vector startpos;
+    Vector endpos;
+    std::byte pad[20];
+    float fraction;
+    int contents;
+    unsigned short dispFlags;
+    bool allSolid;
+    bool startSolid;
+    std::byte pad1[4];
+    struct Surface {
+        const char* name;
+        short surfaceProps;
+        unsigned short flags;
+    } surface;
+    int hitgroup;
+    std::byte pad2[4];
+    Entity* entity;
+    int hitbox;
+};
+
+class EngineTrace {
+public:
+    constexpr void traceRay(const Ray& ray, unsigned int mask, const TraceFilter& filter, Trace& trace) noexcept
+    {
+        callVirtualMethod<void, const Ray&, unsigned int, const TraceFilter&, Trace&>(this, 5, ray, mask, filter, trace);
+    }
+};
