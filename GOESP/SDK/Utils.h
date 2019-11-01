63aca2e89d9998945ab8c03c45557d6798ed4f9c
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-01 20:31:36 +0100

    Remove packColor() function

--- GOESP/SDK/Utils.h
+++ GOESP/SDK/Utils.h
@@ -9,11 +9,6 @@ constexpr auto callVirtualMethod(void* classBase, int index, Args... args) noexc
     return ((*reinterpret_cast<T(__thiscall***)(void*, Args...)>(classBase))[index])(classBase, args...);
 }
 
-constexpr unsigned packColor(float color[3]) noexcept
-{
-    return 0xFF000000 | unsigned(color[0] * 255) | (unsigned(color[1] * 255) << 8) | (unsigned(color[2] * 255) << 16);
-}
-
 constexpr auto rainbowColor(float time, float speed) noexcept
 {
     return std::make_tuple(std::sin(speed * time) * 0.5f + 0.5f,
