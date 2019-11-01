d992dc41449c6011546925d2971fc1a5f6b3ec8f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-01 19:58:00 +0100

    Add GlobalVars struct

--- GOESP/SDK/GlobalVars.h
+++ GOESP/SDK/GlobalVars.h
@@ -1,1 +1,13 @@
 #pragma once
+
+struct GlobalVars {
+    float realtime;
+    int framecount;
+    float absoluteFrameTime;
+    float pad;
+    float currenttime;
+    float frametime;
+    int maxClients;
+    int tickCount;
+    float intervalPerTick;
+};
