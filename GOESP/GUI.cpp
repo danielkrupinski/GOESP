dd3e4f91ec3c2817be2a2a5fa0a6d4ade81b9f8b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-10 16:35:59 +0100

    Update GUI layout

--- GOESP/GUI.cpp
+++ GOESP/GUI.cpp
@@ -59,184 +59,197 @@ void GUI::render() noexcept
 
     ImGui::SetNextWindowCollapsed(true, ImGui::GetIO().KeysDown[VK_SUBTRACT] ? ImGuiCond_Always : ImGuiCond_FirstUseEver);
 
-    blockInput = ImGui::Begin(windowTitle.str().c_str());
+    blockInput = ImGui::Begin(windowTitle.str().c_str(), nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 
-    static int currentCategory = 0;
-    static int currentItem = 0;
-    static int currentSubItem = 0;
+    if (ImGui::BeginTabBar("##tabbar", ImGuiTabBarFlags_Reorderable)) {
+        ImGui::SameLine(ImGui::GetWindowContentRegionMax().x - 350.0f);
 
-    constexpr auto getConfig = [](int category, int item, int subItem = 0) constexpr noexcept -> Config::Shared& {
-        switch (category) {
-        case 0:
-            return config.players[item];
-        case 1:
-            return config.players[item + 3];
-        case 2:
-            switch (item) {
-            default:
-            case 0:
-                return config.weapons;
-            case 1:
-                return config.pistols[subItem];
-            case 2:
-                return config.smgs[subItem];
-            case 3:
-                return config.rifles[subItem];
-            case 4:
-                return config.sniperRifles[subItem];
-            case 5:
-                return config.shotguns[subItem];
-            case 6:
-                return config.heavy[subItem];
-            }
-        case 3:
-            return config.misc[item];
-        default:
-            return config.players[0];
+        ImGui::TextUnformatted("Build date: " __DATE__ " " __TIME__);
+        ImGui::SameLine(ImGui::GetWindowContentRegionMax().x - 55.0f);
+
+        if (ImGui::Button("Unload")) {
+            PdhCloseQuery(cpuQuery);
+            hooks.restore();
         }
-    };
 
-    if (ImGui::ListBoxHeader("##list", { 155.0f, 300.0f })) {
-        static constexpr std::array categories{ "Allies", "Enemies", "Weapons", "Misc" };
+        if (ImGui::BeginTabItem("ESP")) {
+            static int currentCategory = 0;
+            static int currentItem = 0;
+            static int currentSubItem = 0;
 
-        for (size_t i = 0; i < categories.size(); i++) {
-            if (ImGui::Selectable(categories[i], currentCategory == i && currentItem == 0)) {
-                currentCategory = i;
-                currentItem = 0;
-            }
-
-            if (getConfig(i, 0).enabled)
-                continue;
-
-            constexpr auto getItems = [](int index) noexcept -> std::vector<const char*> {
-                switch (index) {
+            constexpr auto getConfig = [](int category, int item, int subItem = 0) constexpr noexcept -> Config::Shared& {
+                switch (category) {
                 case 0:
+                    return config.players[item];
                 case 1:
-                    return { "Visible", "Occluded" };
+                    return config.players[item + 3];
                 case 2:
-                    return { "Pistols", "SMGs", "Rifles", "Sniper Rifles", "Shotguns", "Heavy" };
+                    switch (item) {
+                    default:
+                    case 0:
+                        return config.weapons;
+                    case 1:
+                        return config.pistols[subItem];
+                    case 2:
+                        return config.smgs[subItem];
+                    case 3:
+                        return config.rifles[subItem];
+                    case 4:
+                        return config.sniperRifles[subItem];
+                    case 5:
+                        return config.shotguns[subItem];
+                    case 6:
+                        return config.heavy[subItem];
+                    }
+                case 3:
+                    return config.misc[item];
                 default:
-                    return { "Defuse Kits", "Chickens" };
+                    return config.players[0];
                 }
             };
 
-            ImGui::PushID(i);
-            ImGui::Indent();
+            if (ImGui::ListBoxHeader("##list", { 155.0f, 300.0f })) {
+                static constexpr std::array categories{ "Allies", "Enemies", "Weapons", "Misc" };
 
-            auto items = getItems(i);
-            for (size_t j = 0; j < items.size(); j++) {
-                if (ImGui::Selectable(items[j], currentCategory == i && currentItem == j + 1 && currentSubItem == 0)) {
-                    currentCategory = i;
-                    currentItem = j + 1;
-                    currentSubItem = 0;
-                }
-
-                constexpr auto getSubItems = [](int category, int item) noexcept -> std::vector<const char*> {
-                    switch (category) {
-                    case 2:
-                        switch (item) {
-                        case 0:
-                            return { "Glock-18", "P2000", "USP-S", "Dual Berettas", "P250", "Tec-9", "Five-SeveN", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
-                        case 1:
-                            return { "MAC-10", "MP9", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon" };
-                        case 2:
-                            return { "Galil AR", "FAMAS", "AK-47", "M4A4", "M4A1-S", "SG 553", "AUG" };
-                        case 3:
-                            return { "SSG 08", "AWP", "G3SG1", "SCAR-20" };
-                        case 4:
-                            return { "Nova", "XM1014", "Sawed-Off", "MAG-7" };
-                        case 5:
-                            return { "M249", "Negev" };
-                        }
-                    default:
-                        return { };
-                    }
-                };
-
-                if (getConfig(i, j + 1).enabled)
-                    continue;
-
-                ImGui::Indent();
-                   
-                auto subItems = getSubItems(i, j);
-                for (size_t k = 0; k < subItems.size(); k++) {
-                    if (ImGui::Selectable(subItems[k], currentCategory == i && currentItem == j + 1 && currentSubItem == k + 1)) {
+                for (size_t i = 0; i < categories.size(); i++) {
+                    if (ImGui::Selectable(categories[i], currentCategory == i && currentItem == 0)) {
                         currentCategory = i;
-                        currentItem = j + 1;
-                        currentSubItem = k + 1;
+                        currentItem = 0;
                     }
-                }
 
-                ImGui::Unindent();
+                    if (getConfig(i, 0).enabled)
+                        continue;
+
+                    constexpr auto getItems = [](int index) noexcept -> std::vector<const char*> {
+                        switch (index) {
+                        case 0:
+                        case 1:
+                            return { "Visible", "Occluded" };
+                        case 2:
+                            return { "Pistols", "SMGs", "Rifles", "Sniper Rifles", "Shotguns", "Heavy" };
+                        default:
+                            return { "Defuse Kits", "Chickens" };
+                        }
+                    };
+
+                    ImGui::PushID(i);
+                    ImGui::Indent();
+
+                    auto items = getItems(i);
+                    for (size_t j = 0; j < items.size(); j++) {
+                        if (ImGui::Selectable(items[j], currentCategory == i && currentItem == j + 1 && currentSubItem == 0)) {
+                            currentCategory = i;
+                            currentItem = j + 1;
+                            currentSubItem = 0;
+                        }
+
+                        constexpr auto getSubItems = [](int category, int item) noexcept -> std::vector<const char*> {
+                            switch (category) {
+                            case 2:
+                                switch (item) {
+                                case 0:
+                                    return { "Glock-18", "P2000", "USP-S", "Dual Berettas", "P250", "Tec-9", "Five-SeveN", "CZ75-Auto", "Desert Eagle", "R8 Revolver" };
+                                case 1:
+                                    return { "MAC-10", "MP9", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon" };
+                                case 2:
+                                    return { "Galil AR", "FAMAS", "AK-47", "M4A4", "M4A1-S", "SG 553", "AUG" };
+                                case 3:
+                                    return { "SSG 08", "AWP", "G3SG1", "SCAR-20" };
+                                case 4:
+                                    return { "Nova", "XM1014", "Sawed-Off", "MAG-7" };
+                                case 5:
+                                    return { "M249", "Negev" };
+                                }
+                            default:
+                                return { };
+                            }
+                        };
+
+                        if (getConfig(i, j + 1).enabled)
+                            continue;
+
+                        ImGui::Indent();
+
+                        auto subItems = getSubItems(i, j);
+                        for (size_t k = 0; k < subItems.size(); k++) {
+                            if (ImGui::Selectable(subItems[k], currentCategory == i && currentItem == j + 1 && currentSubItem == k + 1)) {
+                                currentCategory = i;
+                                currentItem = j + 1;
+                                currentSubItem = k + 1;
+                            }
+                        }
+
+                        ImGui::Unindent();
+                    }
+
+                    ImGui::Unindent();
+                    ImGui::PopID();
+                }
+                ImGui::ListBoxFooter();
             }
 
-            ImGui::Unindent();
-            ImGui::PopID();
-        }
-        ImGui::ListBoxFooter();
-    }
-
-    ImGui::SameLine();
-
-    if (ImGui::BeginChild("##child", { 400.0f, 0.0f })) {
-        switch (currentCategory) {
-        case 0:
-        case 1: {
-            auto& playerConfig = config.players[currentCategory * 3 + currentItem];
-
-            ImGui::Checkbox("Enabled", &playerConfig.enabled);
-            ImGui::Separator();
-
-            constexpr auto spacing{ 200.0f };
-            ImGuiCustom::colorPicker("Snaplines", playerConfig.snaplines);
-            ImGui::SameLine(spacing);
-            ImGuiCustom::colorPicker("Box", playerConfig.box);
             ImGui::SameLine();
-            ImGui::SetNextItemWidth(95.0f);
-            ImGui::Combo("", &playerConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
-            break;
+
+            if (ImGui::BeginChild("##child", { 400.0f, 0.0f })) {
+                switch (currentCategory) {
+                case 0:
+                case 1: {
+                    auto& playerConfig = config.players[currentCategory * 3 + currentItem];
+
+                    ImGui::Checkbox("Enabled", &playerConfig.enabled);
+                    ImGui::Separator();
+
+                    constexpr auto spacing{ 200.0f };
+                    ImGuiCustom::colorPicker("Snaplines", playerConfig.snaplines);
+                    ImGui::SameLine(spacing);
+                    ImGuiCustom::colorPicker("Box", playerConfig.box);
+                    ImGui::SameLine();
+                    ImGui::SetNextItemWidth(95.0f);
+                    ImGui::Combo("", &playerConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
+                    break;
+                }
+                case 2: {
+                    auto& weaponConfig = getConfig(currentCategory, currentItem, currentSubItem);
+
+                    ImGui::Checkbox("Enabled", &weaponConfig.enabled);
+                    ImGui::Separator();
+
+                    constexpr auto spacing{ 200.0f };
+                    ImGuiCustom::colorPicker("Snaplines", weaponConfig.snaplines);
+                    ImGui::SameLine(spacing);
+                    ImGuiCustom::colorPicker("Box", weaponConfig.box);
+                    ImGui::SameLine();
+                    ImGui::SetNextItemWidth(95.0f);
+                    ImGui::Combo("", &weaponConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
+                    break;
+                }
+                case 3: {
+                    auto& miscConfig = getConfig(currentCategory, currentItem, currentSubItem);
+
+                    ImGui::Checkbox("Enabled", &miscConfig.enabled);
+                    ImGui::Separator();
+
+                    constexpr auto spacing{ 200.0f };
+                    ImGuiCustom::colorPicker("Snaplines", miscConfig.snaplines);
+                    ImGui::SameLine(spacing);
+                    ImGuiCustom::colorPicker("Box", miscConfig.box);
+                    ImGui::SameLine();
+                    ImGui::SetNextItemWidth(95.0f);
+                    ImGui::Combo("", &miscConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
+                }
+                }
+                ImGui::EndChild();
+            }
+            ImGui::EndTabItem();
         }
-        case 2: {
-            auto& weaponConfig = getConfig(currentCategory, currentItem, currentSubItem);
-
-            ImGui::Checkbox("Enabled", &weaponConfig.enabled);
-            ImGui::Separator();
-
-            constexpr auto spacing{ 200.0f };
-            ImGuiCustom::colorPicker("Snaplines", weaponConfig.snaplines);
-            ImGui::SameLine(spacing);
-            ImGuiCustom::colorPicker("Box", weaponConfig.box);
-            ImGui::SameLine();
-            ImGui::SetNextItemWidth(95.0f);
-            ImGui::Combo("", &weaponConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
-            break;
+        if (ImGui::BeginTabItem("Fonts")) {
+            ImGui::TextUnformatted("Fonts here...");
+            ImGui::EndTabItem();
         }
-        case 3: {
-            auto& miscConfig = getConfig(currentCategory, currentItem, currentSubItem);
-
-            ImGui::Checkbox("Enabled", &miscConfig.enabled);
-            ImGui::Separator();
-
-            constexpr auto spacing{ 200.0f };
-            ImGuiCustom::colorPicker("Snaplines", miscConfig.snaplines);
-            ImGui::SameLine(spacing);
-            ImGuiCustom::colorPicker("Box", miscConfig.box);
-            ImGui::SameLine();
-            ImGui::SetNextItemWidth(95.0f);
-            ImGui::Combo("", &miscConfig.boxType, "2D\0" "2D corners\0" "3D\0" "3D corners\0");
+        if (ImGui::BeginTabItem("Configs")) {
+            ImGui::TextUnformatted("Configs here...");
+            ImGui::EndTabItem();
         }
-        }
-        ImGui::EndChild();
-    }
-
-    ImGui::Separator();
-
-    ImGui::TextUnformatted("Build date: " __DATE__ " " __TIME__);
-    ImGui::SameLine(0.0f, 30.0f);
-
-    if (ImGui::Button("Unload")) {
-        PdhCloseQuery(cpuQuery);
-        hooks.restore();
     }
 
     ImGui::End();
