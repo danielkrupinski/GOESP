1ee386844c571a8ddb9885c0c72252639bde9dff
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-14 19:05:53 +0100

    Add WeaponData struct

--- GOESP/SDK/WeaponData.h
+++ GOESP/SDK/WeaponData.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <cstddef>
+
+struct WeaponData {
+private: std::byte pad_0[20];
+public:
+    int maxClip;
+private: std::byte pad_1[112];
+public:
+    const char* name;
+private: std::byte pad_2[60];
+public:
+    int type;
+private: std::byte pad_3[32];
+    bool fullAuto;
+private: std::byte pad_4[3];
+public:
+    int damage;
+    float armorRatio;
+    int bullets;
+    float penetration;
+private: std::byte pad4[8];
+public:
+    float range;
+    float rangeModifier;
+private: std::byte pad5[16];
+public:
+    bool hasSilencer;
+};
