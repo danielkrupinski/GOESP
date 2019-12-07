e43f1a53a19090086682969fafadb9cd396043bd
Daniel Krupiński <danielkrupinski@outlook.com>
2019-12-07 19:14:21 +0100

    Add Cvar class definition

--- GOESP/SDK/Cvar.h
+++ GOESP/SDK/Cvar.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Utils.h"
+
+struct ConVar;
+
+class Cvar {
+public:
+    constexpr auto findVar(const char* name) noexcept
+    {
+        return callVirtualMethod<ConVar*, const char*>(this, 15, name);
+    }
+};
