10c6bb786ed60186c795dd41bc85d5a87b9f86a6
Daniel Krupiński <danielkrupinski@outlook.com>
2019-12-27 19:28:52 +0100

    Update Memory class constructor

--- GOESP/Memory.cpp
+++ GOESP/Memory.cpp
@@ -10,10 +10,10 @@ static constexpr auto relativeToAbsolute(uintptr_t address) noexcept
 
 Memory::Memory() noexcept
 {
-    present = findPattern<>(L"gameoverlayrenderer", "\xFF\x15????\x8B\xF8\x85\xDB", 2);
-    reset = findPattern<>(L"gameoverlayrenderer", "\xC7\x45?????\xFF\x15????\x8B\xF8", 9);
+    present = findPattern(L"gameoverlayrenderer", "\xFF\x15????\x8B\xF8\x85\xDB", 2);
+    reset = findPattern(L"gameoverlayrenderer", "\xC7\x45?????\xFF\x15????\x8B\xF8", 9);
 
-    isOtherEnemy = relativeToAbsolute<decltype(isOtherEnemy)>(findPattern<>(L"client_panorama", "\xE8????\x02\xC0", 1));
+    isOtherEnemy = relativeToAbsolute<decltype(isOtherEnemy)>(findPattern(L"client_panorama", "\xE8????\x02\xC0", 1));
     globalVars = **reinterpret_cast<GlobalVars***>((*reinterpret_cast<uintptr_t**>(interfaces->client))[11] + 10);
     debugMsg = reinterpret_cast<decltype(debugMsg)>(GetProcAddress(GetModuleHandleW(L"tier0"), "Msg"));
 }
