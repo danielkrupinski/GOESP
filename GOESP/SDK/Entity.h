00557ac1976ea4b574f886cf19654b32d4a560fe
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-06 20:48:00 +0100

    Update Entity.h to use VIRTUAL_METHOD macro

--- GOESP/SDK/Entity.h
+++ GOESP/SDK/Entity.h
@@ -32,61 +32,23 @@ std::add_lvalue_reference_t<type> func_name() noexcept \
 
 class Entity {
 public:
-    constexpr ClientClass* getClientClass() noexcept
-    {
-        return VirtualMethod::call<ClientClass*, 2>(this + 8);
-    }
+    VIRTUAL_METHOD_(getClientClass, ClientClass*, 2, this + 8)
+    VIRTUAL_METHOD_(isDormant, bool, 9, this + 8)
+    VIRTUAL_METHOD_(getModel, const Model*, 8, this + 4)
 
-    constexpr auto isDormant() noexcept
-    {
-        return VirtualMethod::call<bool, 9>(this + 8);
-    }
-
-    constexpr auto getModel() noexcept
-    {
-        return VirtualMethod::call<const Model*, 8>(this + 4);
-    }
-
-    constexpr auto getCollideable() noexcept
-    {
-        return VirtualMethod::call<Collideable*, 3>(this);
-    }
-
-    constexpr auto getAbsOrigin() noexcept
-    {
-        return VirtualMethod::call<Vector&, 10>(this);
-    }
-
-    constexpr auto isAlive() noexcept
-    {
-        return VirtualMethod::call<bool, 155>(this);
-    }
-
-    constexpr auto isWeapon() noexcept
-    {
-        return VirtualMethod::call<bool, 165>(this);
-    }
-
-    constexpr auto getActiveWeapon() noexcept
-    {
-        return VirtualMethod::call<Entity*, 267>(this);
-    }
-
+    VIRTUAL_METHOD(getCollideable, Collideable*, 3)
+    VIRTUAL_METHOD(getAbsOrigin, Vector&, 10)
+    VIRTUAL_METHOD(isAlive, bool, 155)
+    VIRTUAL_METHOD(isWeapon, bool, 165)
+    VIRTUAL_METHOD(getActiveWeapon, Entity*, 267)
+    VIRTUAL_METHOD(getObserverTarget, Entity*, 294)
+    VIRTUAL_METHOD(getWeaponInfo, WeaponInfo*, 459)
+   
     auto getEyePosition() noexcept
     {
         Vector vec;
         VirtualMethod::call<void, 284>(this, std::ref(vec));
         return vec;
-    }
-
-    constexpr auto getObserverTarget() noexcept
-    {
-        return VirtualMethod::call<Entity*, 294>(this);
-    }
-
-    constexpr auto getWeaponInfo() noexcept
-    {
-        return VirtualMethod::call<WeaponInfo*, 459>(this);
     }
 
     bool visibleTo(Entity* other) noexcept
