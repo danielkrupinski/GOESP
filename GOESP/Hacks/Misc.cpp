5f5da7f2300249166ec0b4ccd0619f3a84afed66
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-07 19:07:09 +0100

    Update Misc::drawRecoilCrosshair() function

--- GOESP/Hacks/Misc.cpp
+++ GOESP/Hacks/Misc.cpp
@@ -44,7 +44,7 @@ void Misc::collectData() noexcept
             localPlayer.inReload = false;
             localPlayer.nextWeaponAttack = 0.0f;
         }
-        localPlayer.aimPunch = local->aimPunchAngle();
+        localPlayer.aimPunch = local->getAimPunch();
     }
 }
 
@@ -75,22 +75,20 @@ void Misc::drawReloadProgress(ImDrawList* drawList) noexcept
 
 void Misc::drawRecoilCrosshair(ImDrawList* drawList) noexcept
 {
-    if (config->recoilCrosshair.enabled && interfaces->engine->isInGame()) {
-        std::scoped_lock _{ dataMutex };
+    if (!config->recoilCrosshair.enabled)
+        return;
 
-        if (!(localPlayer.exists && localPlayer.alive))
-            return;
+    std::scoped_lock _{ dataMutex };
 
-        static auto weaponRecoilScale = interfaces->cvar->findVar("weapon_recoil_scale");
+    if (!localPlayer.exists || !localPlayer.alive)
+        return;
 
-        const auto punchAngle = localPlayer.aimPunch * weaponRecoilScale->getFloat();
-        const auto [width, height] = interfaces->engine->getScreenSize();
+    const auto [width, height] = interfaces->engine->getScreenSize();
 
-        const float x = width * (0.5f - punchAngle.y / 180.0f);
-        const float y = height * (0.5f + punchAngle.x / 180.0f);
-        const auto color = Helpers::calculateColor(config->recoilCrosshair.color, config->recoilCrosshair.rainbow, config->recoilCrosshair.rainbowSpeed, memory->globalVars->realtime);
+    const float x = width * (0.5f - localPlayer.aimPunch.y / 180.0f);
+    const float y = height * (0.5f + localPlayer.aimPunch.x / 180.0f);
+    const auto color = Helpers::calculateColor(config->recoilCrosshair.color, config->recoilCrosshair.rainbow, config->recoilCrosshair.rainbowSpeed, memory->globalVars->realtime);
 
-        drawList->AddLine({ x, y - 10 }, { x, y + 10 }, color, config->recoilCrosshair.thickness);
-        drawList->AddLine({ x - 10, y }, { x + 10, y }, color, config->recoilCrosshair.thickness);
-    }
+    drawList->AddLine({ x, y - 10 }, { x, y + 10 }, color, config->recoilCrosshair.thickness);
+    drawList->AddLine({ x - 10, y }, { x + 10, y }, color, config->recoilCrosshair.thickness);
 }
