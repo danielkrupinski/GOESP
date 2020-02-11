50fe7f991226837b3848d3a86d0c714faae8ab1d
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-11 17:25:10 +0100

    Update dllmain.cpp

--- GOESP/dllmain.cpp
+++ GOESP/dllmain.cpp
@@ -1,16 +1,10 @@
 #include <Windows.h>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_dx9.h"
-#include "imgui/imgui_impl_win32.h"
-
 #include "Config.h"
 #include "GUI.h"
+#include "Hooks.h"
 #include "Interfaces.h"
 #include "Memory.h"
-#include "Hooks.h"
-
-#include "SDK/InputSystem.h"
 
 static WNDPROC originalWndproc;
 static HMODULE module;
@@ -18,9 +12,9 @@ static HMODULE module;
 static LRESULT WINAPI init(HWND window, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
 {
     SetWindowLongPtr(FindWindowW(L"Valve001", nullptr), GWLP_WNDPROC, LONG_PTR(originalWndproc));
+    interfaces = std::make_unique<const Interfaces>();
     config = std::make_unique<Config>("GOESP");
     gui = std::make_unique<GUI>();
-    interfaces = std::make_unique<const Interfaces>();
     memory = std::make_unique<Memory>();
     hooks = std::make_unique<Hooks>(module);
 
