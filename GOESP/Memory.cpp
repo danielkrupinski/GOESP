69dbb9e55e7c2ccd60b9fce914d2dd348bce92a5
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-12 21:53:20 +0100

    Update Memory::findPattern() method

--- GOESP/Memory.cpp
+++ GOESP/Memory.cpp
@@ -12,7 +12,7 @@ Memory::Memory() noexcept
 {
     present = findPattern(L"gameoverlayrenderer", "\xFF\x15????\x8B\xF8\x85\xDB", 2);
     reset = findPattern(L"gameoverlayrenderer", "\xC7\x45?????\xFF\x15????\x8B\xF8", 9);
-    setCursorPos = *findPattern<uintptr_t*>(L"gameoverlayrenderer", "\xC2\x08?\x5D", 6);
+    setCursorPos = *reinterpret_cast<std::uintptr_t*>(findPattern(L"gameoverlayrenderer", "\xC2\x08?\x5D", 6));
 
     isOtherEnemy = relativeToAbsolute<decltype(isOtherEnemy)>(findPattern(L"client_panorama", "\xE8????\x02\xC0", 1));
     globalVars = **reinterpret_cast<GlobalVars***>((*reinterpret_cast<uintptr_t**>(interfaces->client))[11] + 10);
--- GOESP/Memory.h
+++ GOESP/Memory.h
@@ -23,9 +23,11 @@ public:
     const GlobalVars* globalVars;
     std::add_pointer_t<void __cdecl(const char* msg, ...)> debugMsg;
 private:
-    template <typename T = uintptr_t>
-    static auto findPattern(const wchar_t* module, const char* pattern, size_t offset = 0) noexcept
+    static std::uintptr_t findPattern(const wchar_t* module, const char* pattern, size_t offset = 0) noexcept
     {
+        static auto id = 0;
+        ++id;
+
         if (MODULEINFO moduleInfo; GetModuleInformation(GetCurrentProcess(), GetModuleHandleW(module), &moduleInfo, sizeof(moduleInfo))) {
             auto start = static_cast<const char*>(moduleInfo.lpBaseOfDll);
             const auto end = start + moduleInfo.SizeOfImage;
@@ -35,8 +37,8 @@ private:
 
             while (first < end && *second) {
                 if (*first == *second || *second == '?') {
-                    first++;
-                    second++;
+                    ++first;
+                    ++second;
                 } else {
                     first = ++start;
                     second = pattern;
@@ -44,10 +46,10 @@ private:
             }
 
             if (!*second)
-                return reinterpret_cast<T>(const_cast<char*>(start) + offset);
+                return reinterpret_cast<std::uintptr_t>(const_cast<char*>(start) + offset);
         }
-        MessageBoxW(NULL, (std::wostringstream{ } << L"Failed to find pattern in " << module << L'!').str().c_str(), L"GOESP", MB_OK | MB_ICONERROR);
-        std::exit(EXIT_FAILURE);
+        MessageBoxA(NULL, ("Failed to find pattern #" + std::to_string(id).append("!")).c_str(), "GOESP", MB_OK | MB_ICONWARNING);
+        return 0;
     }
 };
 
