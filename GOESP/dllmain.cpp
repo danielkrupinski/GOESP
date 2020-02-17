17e287d02742efbd45413b6f248cb0b0b1b784ff
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-17 19:25:58 +0100

    Update dllmain.cpp

--- GOESP/dllmain.cpp
+++ GOESP/dllmain.cpp
@@ -14,10 +14,10 @@ static LRESULT WINAPI init(HWND window, UINT msg, WPARAM wParam, LPARAM lParam) 
 {
     SetWindowLongPtr(FindWindowW(L"Valve001", nullptr), GWLP_WNDPROC, LONG_PTR(originalWndproc));
     interfaces = std::make_unique<const Interfaces>();
+    memory = std::make_unique<Memory>();
     eventListener = std::make_unique<EventListener>();
     config = std::make_unique<Config>("GOESP");
     gui = std::make_unique<GUI>();
-    memory = std::make_unique<Memory>();
     hooks = std::make_unique<Hooks>(module);
 
     return CallWindowProc(originalWndproc, window, msg, wParam, lParam);
@@ -27,7 +27,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         module = hModule;
-        originalWndproc = WNDPROC(SetWindowLongPtrA(FindWindowW(L"Valve001", nullptr), GWLP_WNDPROC, LONG_PTR(init)));
+        originalWndproc = WNDPROC(SetWindowLongPtr(FindWindowW(L"Valve001", nullptr), GWLP_WNDPROC, LONG_PTR(init)));
     }
     return TRUE;
 }
