85b641e417466a62e5cd293e0fbe4b688f8d4760
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-23 11:09:50 +0100

    Call Misc::purchaseList() in present hook

--- GOESP/Hooks.cpp
+++ GOESP/Hooks.cpp
@@ -82,6 +82,7 @@ static HRESULT D3DAPI present(IDirect3DDevice9* device, const RECT* src, const R
     ESP::render(ImGui::GetBackgroundDrawList());
     Misc::drawReloadProgress(ImGui::GetBackgroundDrawList());
     Misc::drawRecoilCrosshair(ImGui::GetBackgroundDrawList());
+    Misc::purchaseList();
 
     gui->render();
 
