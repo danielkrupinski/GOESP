a2bf4044c58656f5dbfcecb431b36ff31f3902a7
Daniel Krupiński <danielkrupinski@outlook.com>
2020-03-11 20:01:05 +0100

    Add WeaponSystem class

--- GOESP/SDK/WeaponSystem.h
+++ GOESP/SDK/WeaponSystem.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "VirtualMethod.h"
+#include "WeaponId.h"
+
+struct WeaponInfo;
+
+class WeaponSystem {
+public:
+    constexpr auto getWeaponInfo(WeaponId weaponId) noexcept
+    {
+        return VirtualMethod::call<WeaponInfo*, 2>(this, weaponId);
+    }
+};
