5b3d723d7b0ac7818a8104184fe822223176e32e
Daniel Krupiński <danielkrupinski@outlook.com>
2020-03-16 20:58:52 +0100

    Rename "Total" mode of "Purchase List" to "Summary"

--- GOESP/GUI.cpp
+++ GOESP/GUI.cpp
@@ -273,7 +273,7 @@ void GUI::render() noexcept
 
         if (ImGui::BeginPopup("##purchaselist")) {
             ImGui::SetNextItemWidth(75.0f);
-            ImGui::Combo("Mode", &config->purchaseList.mode, "Details\0Total\0");
+            ImGui::Combo("Mode", &config->purchaseList.mode, "Details\0Summary\0");
             ImGui::Checkbox("Show Prices", &config->purchaseList.showPrices);
             ImGui::EndPopup();
         }
