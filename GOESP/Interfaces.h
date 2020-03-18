8ddd286eb2c7dda29c7f0c7b86cb43374c13c90a
Daniel Krupiński <danielkrupinski@outlook.com>
2020-03-18 17:44:46 +0100

    Make Interfaces::find() method non-template to avoid duplications in resultant code

--- GOESP/Interfaces.h
+++ GOESP/Interfaces.h
@@ -14,22 +14,24 @@ class GameEventManager;
 class InputSystem;
 class Localize;
 
+#define GAME_INTERFACE(type, name, module, version) \
+type* name = reinterpret_cast<type*>(find(L##module, version));
+
 class Interfaces {
 public:
-    Client* client = find<Client>(L"client_panorama", "VClient018");
-    Cvar* cvar = find<Cvar>(L"vstdlib", "VEngineCvar007");
-    Engine* engine = find<Engine>(L"engine", "VEngineClient014");
-    EngineTrace* engineTrace = find<EngineTrace>(L"engine", "EngineTraceClient004");
-    EntityList* entityList = find<EntityList>(L"client_panorama", "VClientEntityList003");
-    GameEventManager* gameEventManager = find<GameEventManager>(L"engine", "GAMEEVENTSMANAGER002");
-    InputSystem* inputSystem = find<InputSystem>(L"inputsystem", "InputSystemVersion001");
-    Localize* localize = find<Localize>(L"localize", "Localize_001");
+    GAME_INTERFACE(Client, client, "client_panorama", "VClient018")
+    GAME_INTERFACE(Cvar, cvar, "vstdlib", "VEngineCvar007")
+    GAME_INTERFACE(Engine, engine, "engine", "VEngineClient014")
+    GAME_INTERFACE(EngineTrace, engineTrace, "engine", "EngineTraceClient004")
+    GAME_INTERFACE(EntityList, entityList, "client_panorama", "VClientEntityList003")
+    GAME_INTERFACE(GameEventManager, gameEventManager, "engine", "GAMEEVENTSMANAGER002")
+    GAME_INTERFACE(InputSystem, inputSystem, "inputsystem", "InputSystemVersion001")
+    GAME_INTERFACE(Localize, localize, "localize", "Localize_001")
 private:
-    template <typename T>
-    static auto find(const wchar_t* module, const char* name) noexcept
+    static void* find(const wchar_t* module, const char* name) noexcept
     {
-        if (const auto createInterface = reinterpret_cast<std::add_pointer_t<T* __cdecl (const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleW(module), "CreateInterface")))
-            if (T* foundInterface = createInterface(name, nullptr))
+        if (const auto createInterface = reinterpret_cast<std::add_pointer_t<void* __cdecl (const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleW(module), "CreateInterface")))
+            if (void* foundInterface = createInterface(name, nullptr))
                 return foundInterface;
 
         MessageBoxA(nullptr, ("Failed to find " + std::string{ name } + " interface!").c_str(), "GOESP", MB_OK | MB_ICONERROR);
