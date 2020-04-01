2c7e8e294c31e74a83fd2a896a833e43bfc77846
Daniel Krupiński <danielkrupinski@outlook.com>
2020-04-01 20:18:07 +0200

    Add bombZoneHint bool to Config class

--- GOESP/Config.h
+++ GOESP/Config.h
@@ -18,33 +18,16 @@ public:
     void load() noexcept;
     void save() noexcept;
 
-    // Legacy
-    /*
-    std::array<Player, 6> players;
-    Weapon weapons;
-    std::array<Weapon, 11> pistols;
-    std::array<Weapon, 8> smgs;
-    std::array<Weapon, 8> rifles;
-    std::array<Weapon, 5> sniperRifles;
-    std::array<Weapon, 7> shotguns;
-    std::array<Weapon, 3> machineguns;
-    std::array<Weapon, 12> grenades;
-    std::array<Shared, 10> projectiles;
-    std::array<Shared, 4> otherEntities;
-    */
-
-    //
     std::unordered_map<std::string, Player> allies;
     std::unordered_map<std::string, Player> enemies;
     std::unordered_map<std::string, Weapon> _weapons;
     std::unordered_map<std::string, Projectile> _projectiles;
     std::unordered_map<std::string, Shared> _otherEntities;
-    //
 
     ColorToggleThickness reloadProgress{ 5.0f };
     ColorToggleThickness recoilCrosshair;
     bool normalizePlayerNames = true;
-
+    bool bombZoneHint = false;
     PurchaseList purchaseList;
 
     std::vector<std::string> systemFonts{ "Default" };
