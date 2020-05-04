1107084e13b2f33b67873014a76cfa3e1b8c2e5e
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-04 15:33:43 +0200

    Fill SDK/ModelInfo.h with structs definitions

--- GOESP/SDK/ModelInfo.h
+++ GOESP/SDK/ModelInfo.h
@@ -1,1 +1,75 @@
 #pragma once
+
+#include <cstdint>
+
+#include "Vector.h"
+#include "VirtualMethod.h"
+
+struct StudioBbox {
+    int bone;
+    int group;
+    Vector bbMin;
+    Vector bbMax;
+    int hitboxNameIndex;
+    Vector offsetOrientation;
+    float capsuleRadius;
+    int	unused[4];
+};
+
+struct StudioHitboxSet {
+    int nameIndex;
+    int numHitboxes;
+    int hitboxIndex;
+
+    const char* getName() noexcept
+    {
+        return nameIndex ? reinterpret_cast<const char*>(std::uintptr_t(this) + nameIndex) : nullptr;
+    }
+
+    StudioBbox* getHitbox(int i) noexcept
+    {
+        return i >= 0 && i < numHitboxes ? reinterpret_cast<StudioBbox*>(std::uintptr_t(this) + hitboxIndex) + i : nullptr;
+    }
+};
+
+struct StudioHdr {
+    int id;
+    int version;
+    int checksum;
+    char name[64];
+    int length;
+    Vector eyePosition;
+    Vector illumPosition;
+    Vector hullMin;
+    Vector hullMax;
+    Vector bbMin;
+    Vector bbMax;
+    int flags;
+    int numBones;
+    int boneIndex;
+    int numBoneControllers;
+    int boneControllerIndex;
+    int numHitboxSets;
+    int hitboxSetIndex;
+
+    StudioHitboxSet* getHitboxSet(int i) noexcept
+    {
+        return i >= 0 && i < numHitboxSets ? reinterpret_cast<StudioHitboxSet*>(std::uintptr_t(this) + hitboxSetIndex) + i : nullptr;
+    }
+};
+
+struct Model {
+    void* handle;
+    char name[260];
+    int	loadFlags;
+    int	serverCount;
+    int	type;
+    int	flags;	
+    Vector mins, maxs;
+};
+
+class ModelInfo {
+public:
+    VIRTUAL_METHOD(int, getModelIndex, 2, (const char* name), (this, name))
+    VIRTUAL_METHOD(StudioHdr*, getStudioModel, 32, (const Model* model), (this, model))
+};
