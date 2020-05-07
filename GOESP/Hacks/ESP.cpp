136bd8fbbf155c65f901d62e7686920ac91a1034
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-07 12:40:59 +0200

    Update renderBox() function to take Box struct

--- GOESP/Hacks/ESP.cpp
+++ GOESP/Hacks/ESP.cpp
@@ -488,36 +488,36 @@ public:
     }
 };
 
-static void renderBox(ImDrawList* drawList, const BoundingBox& bbox, const Shared& config) noexcept
+static void renderBox(ImDrawList* drawList, const BoundingBox& bbox, const Box& config) noexcept
 {
-    if (!config.box.enabled)
+    if (!config.enabled)
         return;
 
-    const ImU32 color = Helpers::calculateColor(config.box, memory->globalVars->realtime);
+    const ImU32 color = Helpers::calculateColor(config, memory->globalVars->realtime);
 
-    switch (config.box.type) {
+    switch (config.type) {
     case 0:
-        drawList->AddRect(bbox.min, bbox.max, color, config.box.rounding, ImDrawCornerFlags_All, config.box.thickness);
+        drawList->AddRect(bbox.min, bbox.max, color, config.rounding, ImDrawCornerFlags_All, config.thickness);
         break;
 
     case 1:
-        drawList->AddLine(bbox.min, { bbox.min.x, bbox.min.y * 0.75f + bbox.max.y * 0.25f }, color, config.box.thickness);
-        drawList->AddLine(bbox.min, { bbox.min.x * 0.75f + bbox.max.x * 0.25f, bbox.min.y }, color, config.box.thickness);
+        drawList->AddLine(bbox.min, { bbox.min.x, bbox.min.y * 0.75f + bbox.max.y * 0.25f }, color, config.thickness);
+        drawList->AddLine(bbox.min, { bbox.min.x * 0.75f + bbox.max.x * 0.25f, bbox.min.y }, color, config.thickness);
 
-        drawList->AddLine({ bbox.max.x, bbox.min.y }, { bbox.max.x * 0.75f + bbox.min.x * 0.25f, bbox.min.y }, color, config.box.thickness);
-        drawList->AddLine({ bbox.max.x, bbox.min.y }, { bbox.max.x, bbox.min.y * 0.75f + bbox.max.y * 0.25f }, color, config.box.thickness);
+        drawList->AddLine({ bbox.max.x, bbox.min.y }, { bbox.max.x * 0.75f + bbox.min.x * 0.25f, bbox.min.y }, color, config.thickness);
+        drawList->AddLine({ bbox.max.x, bbox.min.y }, { bbox.max.x, bbox.min.y * 0.75f + bbox.max.y * 0.25f }, color, config.thickness);
 
-        drawList->AddLine({ bbox.min.x, bbox.max.y }, { bbox.min.x, bbox.max.y * 0.75f + bbox.min.y * 0.25f }, color, config.box.thickness);
-        drawList->AddLine({ bbox.min.x, bbox.max.y }, { bbox.min.x * 0.75f + bbox.max.x * 0.25f, bbox.max.y }, color, config.box.thickness);
+        drawList->AddLine({ bbox.min.x, bbox.max.y }, { bbox.min.x, bbox.max.y * 0.75f + bbox.min.y * 0.25f }, color, config.thickness);
+        drawList->AddLine({ bbox.min.x, bbox.max.y }, { bbox.min.x * 0.75f + bbox.max.x * 0.25f, bbox.max.y }, color, config.thickness);
 
-        drawList->AddLine(bbox.max, { bbox.max.x * 0.75f + bbox.min.x * 0.25f, bbox.max.y }, color, config.box.thickness);
-        drawList->AddLine(bbox.max, { bbox.max.x, bbox.max.y * 0.75f + bbox.min.y * 0.25f }, color, config.box.thickness);
+        drawList->AddLine(bbox.max, { bbox.max.x * 0.75f + bbox.min.x * 0.25f, bbox.max.y }, color, config.thickness);
+        drawList->AddLine(bbox.max, { bbox.max.x, bbox.max.y * 0.75f + bbox.min.y * 0.25f }, color, config.thickness);
         break;
     case 2:
         for (int i = 0; i < 8; ++i) {
             for (int j = 1; j <= 4; j <<= 1) {
                 if (!(i & j))
-                    drawList->AddLine(bbox.vertices[i], bbox.vertices[i + j], color, config.box.thickness);
+                    drawList->AddLine(bbox.vertices[i], bbox.vertices[i + j], color, config.thickness);
             }
         }
         break;
@@ -525,8 +525,8 @@ static void renderBox(ImDrawList* drawList, const BoundingBox& bbox, const Share
         for (int i = 0; i < 8; ++i) {
             for (int j = 1; j <= 4; j <<= 1) {
                 if (!(i & j)) {
-                    drawList->AddLine(bbox.vertices[i], { bbox.vertices[i].x * 0.75f + bbox.vertices[i + j].x * 0.25f, bbox.vertices[i].y * 0.75f + bbox.vertices[i + j].y * 0.25f }, color, config.box.thickness);
-                    drawList->AddLine({ bbox.vertices[i].x * 0.25f + bbox.vertices[i + j].x * 0.75f, bbox.vertices[i].y * 0.25f + bbox.vertices[i + j].y * 0.75f }, bbox.vertices[i + j], color, config.box.thickness);
+                    drawList->AddLine(bbox.vertices[i], { bbox.vertices[i].x * 0.75f + bbox.vertices[i + j].x * 0.25f, bbox.vertices[i].y * 0.75f + bbox.vertices[i + j].y * 0.25f }, color, config.thickness);
+                    drawList->AddLine({ bbox.vertices[i].x * 0.25f + bbox.vertices[i + j].x * 0.75f, bbox.vertices[i].y * 0.25f + bbox.vertices[i + j].y * 0.75f }, bbox.vertices[i + j], color, config.thickness);
                 }
             }
         }
@@ -583,7 +583,7 @@ static void renderPlayerBox(ImDrawList* drawList, const PlayerData& playerData, 
     if (!bbox)
         return;
 
-    renderBox(drawList, bbox, config);
+    renderBox(drawList, bbox, config.box);
     renderSnaplines(drawList, bbox, config.snapline);
 
     ImVec2 flashDurationPos{ (bbox.min.x + bbox.max.x) / 2, bbox.min.y - 12.5f };
@@ -610,7 +610,7 @@ static void renderWeaponBox(ImDrawList* drawList, const WeaponData& weaponData, 
     if (!bbox)
         return;
 
-    renderBox(drawList, bbox, config);
+    renderBox(drawList, bbox, config.box);
     renderSnaplines(drawList, bbox, config.snapline);
 
     if (config.name.enabled && !weaponData.displayName.empty()) {
@@ -630,7 +630,7 @@ static void renderEntityBox(ImDrawList* drawList, const BaseData& entityData, co
     if (!bbox)
         return;
 
-    renderBox(drawList, bbox, config);
+    renderBox(drawList, bbox, config.box);
     renderSnaplines(drawList, bbox, config.snapline);
 
     if (config.name.enabled)
