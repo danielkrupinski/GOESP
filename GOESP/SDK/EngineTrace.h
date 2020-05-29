51d623007b685d6069da781993d0595598b2af03
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-29 14:14:30 +0200

    Name second argument of TraceFilter::shouldHitEntity() method

--- GOESP/SDK/EngineTrace.h
+++ GOESP/SDK/EngineTrace.h
@@ -20,7 +20,7 @@ class Entity;
 
 struct TraceFilter {
     TraceFilter(const Entity* entity) : skip{ entity } { }
-    virtual bool shouldHitEntity(Entity* entity, int) { return entity != skip; }
+    virtual bool shouldHitEntity(Entity* entity, int contentsMask) { return entity != skip; }
     virtual int getTraceType() const { return 0; }
     const void* skip;
 };
