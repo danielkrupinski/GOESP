6c4c06c8f8142398372d1b826d4e5f6facf2561a
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-09 13:15:26 +0200

    Remove [[nodiscard]] attribute from  VIRTUAL_METHOD macro

--- GOESP/SDK/VirtualMethod.h
+++ GOESP/SDK/VirtualMethod.h
@@ -12,7 +12,7 @@ namespace VirtualMethod
 }
 
 #define VIRTUAL_METHOD(returnType, name, idx, args, argsRaw) \
-[[nodiscard]] constexpr auto name##args noexcept \
+constexpr auto name##args noexcept \
 { \
     return VirtualMethod::call<returnType, idx>##argsRaw; \
 }
