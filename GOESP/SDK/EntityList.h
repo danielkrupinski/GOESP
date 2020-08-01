d3852f572a953a3924ee05d899a6e38f141c404a
Daniel Krupiński <danielkrupinski@outlook.com>
2020-08-01 21:56:13 +0200

    Add linux implementation of EntityList class

--- GOESP/SDK/EntityList.h
+++ GOESP/SDK/EntityList.h
@@ -6,7 +6,13 @@ class Entity;
 
 class EntityList {
 public:
+#ifdef _WIN32
     VIRTUAL_METHOD(Entity*, getEntity, 3, (int index), (this, index))
     VIRTUAL_METHOD(Entity*, getEntityFromHandle, 4, (int handle), (this, handle))
     VIRTUAL_METHOD(int, getHighestEntityIndex, 6, (), (this))
+#elif __linux__
+    VIRTUAL_METHOD(Entity*, getEntity, 6, (int index), (this, index))
+    VIRTUAL_METHOD(Entity*, getEntityFromHandle, 10, (int handle), (this, handle))
+    VIRTUAL_METHOD(int, getHighestEntityIndex, 11, (), (this))
+#endif
 };
