fc9ae31f25f32e9798402b0103b662ff5811a3f7
Daniel Krupiński <danielkrupinski@outlook.com>
2020-08-02 16:18:43 +0200

    Update linux implementation of EntityList class

--- GOESP/SDK/EntityList.h
+++ GOESP/SDK/EntityList.h
@@ -6,13 +6,11 @@ class Entity;
 
 class EntityList {
 public:
-#ifdef _WIN32
     VIRTUAL_METHOD(Entity*, getEntity, 3, (int index), (this, index))
+#ifdef _WIN32
     VIRTUAL_METHOD(Entity*, getEntityFromHandle, 4, (int handle), (this, handle))
-    VIRTUAL_METHOD(int, getHighestEntityIndex, 6, (), (this))
 #elif __linux__
-    VIRTUAL_METHOD(Entity*, getEntity, 6, (int index), (this, index))
-    VIRTUAL_METHOD(Entity*, getEntityFromHandle, 10, (int handle), (this, handle))
-    VIRTUAL_METHOD(int, getHighestEntityIndex, 11, (), (this))
+    VIRTUAL_METHOD(Entity*, getEntityFromHandle, 4, (int handle), (this, &handle))
 #endif
+    VIRTUAL_METHOD(int, getHighestEntityIndex, 6, (), (this))
 };
