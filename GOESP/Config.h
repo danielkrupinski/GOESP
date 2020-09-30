bd734190b298a55f9547f6e2281e2898ac65d18d
Daniel Krupiński <danielkrupinski@outlook.com>
2020-09-30 19:43:49 +0200

    Config: Make fonts map private, accessible via getFonts() method

--- GOESP/Config.h
+++ GOESP/Config.h
@@ -43,14 +43,14 @@ public:
         ImFont* big;
     };
 
-    std::unordered_map<std::string, Font> fonts;
-
     void scheduleFontLoad(std::size_t index) noexcept;
     bool loadScheduledFonts() noexcept;
     const auto& getSystemFonts() noexcept { return systemFonts; }
+    const auto& getFonts() noexcept { return fonts; }
 private:
     std::vector<std::size_t> scheduledFonts{ 0 };
     std::vector<std::string> systemFonts{ "Default" };
+    std::unordered_map<std::string, Font> fonts;
     std::filesystem::path path;
 };
 
