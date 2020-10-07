aefca200a6979530f4c9f434926a59d88a88f010
Daniel Krupiński <danielkrupinski@outlook.com>
2020-10-07 20:50:55 +0200

    Update EntityList.h for macOS support

--- GOESP/SDK/EntityList.h
+++ GOESP/SDK/EntityList.h
@@ -9,7 +9,7 @@ public:
     VIRTUAL_METHOD(Entity*, getEntity, 3, (int index), (this, index))
 #ifdef _WIN32
     VIRTUAL_METHOD(Entity*, getEntityFromHandle, 4, (int handle), (this, handle))
-#elif __linux__
+#else
     VIRTUAL_METHOD(Entity*, getEntityFromHandle, 4, (int handle), (this, &handle))
 #endif
     VIRTUAL_METHOD(int, getHighestEntityIndex, 6, (), (this))
