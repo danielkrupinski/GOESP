42465448446e8c39d4a3a4a439c2af62a77a0f71
Daniel Krupiński <danielkrupinski@outlook.com>
2020-10-23 17:23:53 +0200

    Extract Memory::getModuleInformation() and Memory::findPattern() outside of Memory class

--- GOESP/Memory.h
+++ GOESP/Memory.h
@@ -6,15 +6,6 @@
 #include <type_traits>
 #include <vector>
 
-#ifdef _WIN32
-#include <Windows.h>
-#include <Psapi.h>
-#elif __linux__
-#include <link.h>
-#elif __APPLE__
-#include <mach-o/dyld.h>
-#endif
-
 #include "SDK/Platform.h"
 
 class Entity;
@@ -59,72 +50,6 @@ public:
     std::uintptr_t swapWindow;
     std::uintptr_t warpMouseInWindow;
 #endif
-private:
-    static std::pair<void*, std::size_t> getModuleInformation(const char* name) noexcept
-    {
-#ifdef _WIN32
-        if (HMODULE handle = GetModuleHandleA(name)) {
-            if (MODULEINFO moduleInfo; GetModuleInformation(GetCurrentProcess(), handle, &moduleInfo, sizeof(moduleInfo)))
-                return std::make_pair(moduleInfo.lpBaseOfDll, moduleInfo.SizeOfImage);
-        }
-        return {};
-#elif __linux__
-        struct ModuleInfo {
-            const char* name;
-            void* base = nullptr;
-            std::size_t size = 0;
-        } moduleInfo;
-
-        moduleInfo.name = name;
-
-        dl_iterate_phdr([](struct dl_phdr_info* info, std::size_t, void* data) {
-            const auto moduleInfo = reinterpret_cast<ModuleInfo*>(data);
-       	    if (std::string_view{ info->dlpi_name }.ends_with(moduleInfo->name)) {
-                moduleInfo->base = (void*)(info->dlpi_addr + info->dlpi_phdr[0].p_vaddr);
-                moduleInfo->size = info->dlpi_phdr[0].p_memsz;
-                return 1;
-       	    }
-            return 0;
-        }, &moduleInfo);
-            
-        return std::make_pair(moduleInfo.base, moduleInfo.size);
-#elif __APPLE__
-        return {};
-#endif
-    }
-
-    static std::uintptr_t findPattern(const char* module, const char* pattern) noexcept
-    {
-        static auto id = 0;
-        ++id;
-
-        const auto [moduleBase, moduleSize] = getModuleInformation(module);
-
-        if (moduleBase && moduleSize) {
-            auto start = static_cast<const char*>(moduleBase);
-            const auto end = start + moduleSize;
-
-            auto first = start;
-            auto second = pattern;
-
-            while (first < end && *second) {
-                if (*first == *second || *second == '?') {
-                    ++first;
-                    ++second;
-                } else {
-                    first = ++start;
-                    second = pattern;
-                }
-            }
-
-            if (!*second)
-                return reinterpret_cast<std::uintptr_t>(start);
-        }
-#ifdef _WIN32
-        MessageBoxA(NULL, ("Failed to find pattern #" + std::to_string(id) + '!').c_str(), "GOESP", MB_OK | MB_ICONWARNING);
-#endif
-        return 0;
-    }
 };
 
 inline std::unique_ptr<const Memory> memory;
