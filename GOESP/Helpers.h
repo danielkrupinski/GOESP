4ba8017696612cd65dc399d34e0d85918c2373ec
Daniel Krupiński <danielkrupinski@outlook.com>
2020-10-25 19:41:59 +0100

    Update Helpers::deg2rad() and Helpers::rad2deg() functions for easier compile-time constant computation

--- GOESP/Helpers.h
+++ GOESP/Helpers.h
@@ -19,6 +19,6 @@ namespace Helpers
 
     ImWchar* getFontGlyphRanges() noexcept;
 
-    constexpr auto deg2rad(float degrees) noexcept { return degrees * std::numbers::pi_v<float> / 180.0f; }
-    constexpr auto rad2deg(float radians) noexcept { return radians * 180.0f / std::numbers::pi_v<float>; }
+    constexpr auto deg2rad(float degrees) noexcept { return degrees * (std::numbers::pi_v<float> / 180.0f); }
+    constexpr auto rad2deg(float radians) noexcept { return radians * (180.0f / std::numbers::pi_v<float>); }
 }
