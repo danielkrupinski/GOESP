10145c5ade8410edc0f14c09b84c795681f760be
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-03 21:08:34 +0100

    Fix health bar alpha not being reduced by flashbang (thanks @playday3008)

--- GOESP/Helpers.cpp
+++ GOESP/Helpers.cpp
@@ -33,6 +33,8 @@ unsigned int Helpers::calculateColor(Color color) noexcept
 
 unsigned int Helpers::calculateColor(int r, int g, int b, int a) noexcept
 {
+    if (!config->ignoreFlashbang)
+        a -= static_cast<int>(a * GameData::local().flashDuration / 255.0f);
     return IM_COL32(r, g, b, a * alphaFactor);
 }
 
