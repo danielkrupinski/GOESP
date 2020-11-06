735688a122d2bd2a60898a84412bb5ef8f68f3d6
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-06 20:42:21 +0100

    Add SteamAPIContext struct

--- GOESP/SDK/Steam.h
+++ GOESP/SDK/Steam.h
@@ -6,3 +6,10 @@ class SteamFriends {
 public:
 	VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
 };
+
+struct SteamAPIContext {
+	void* steamClient;
+	void* steamUser;
+	SteamFriends* steamFriends;
+	// SteamUtils* steamUtils;
+};
