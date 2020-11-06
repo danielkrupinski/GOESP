65957bae908f1f92b46612b61be2f2e6acfa9ae3
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-06 20:45:51 +0100

    Implement SteamUtils class

--- GOESP/SDK/Steam.h
+++ GOESP/SDK/Steam.h
@@ -7,9 +7,14 @@ public:
 	VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
 };
 
+class SteamUtils {
+public:
+	VIRTUAL_METHOD(bool, getImageRGBA, 6, (int image, std::uint8_t* buff, int buffSize), (this, image, buff, buffSize))
+};
+
 struct SteamAPIContext {
 	void* steamClient;
 	void* steamUser;
 	SteamFriends* steamFriends;
-	// SteamUtils* steamUtils;
+	SteamUtils* steamUtils;
 };
