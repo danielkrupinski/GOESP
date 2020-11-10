014c8bc51d98f49e1f9fc06a4b9c5315ea26632f
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-10 21:09:41 +0100

    Clear texture if initialized when move-assigning

--- GOESP/GameData.h
+++ GOESP/GameData.h
@@ -137,7 +137,7 @@ private:
         Texture(const Texture&) = delete;
         Texture& operator=(const Texture&) = delete;
         Texture(Texture&& other) noexcept : texture{ other.texture } { other.texture = nullptr; }
-        Texture& operator=(Texture&& other) noexcept { texture = other.texture; other.texture = nullptr; return *this; }
+        Texture& operator=(Texture&& other) noexcept { clear(); texture = other.texture; other.texture = nullptr; return *this; }
 
         void init(int width, int height, const std::uint8_t* data) noexcept;
         void clear() noexcept;
