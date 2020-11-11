3c6fbdcfebbe5ad1fdbe2b66cb5cdac55a4be858
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-11 09:33:59 +0100

    Call GameData::clearTextures() in reset hook

--- GOESP/Hooks.cpp
+++ GOESP/Hooks.cpp
@@ -46,6 +46,7 @@ static LRESULT WINAPI wndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lPara
 
 static HRESULT D3DAPI reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* params) noexcept
 {
+    GameData::clearTextures();
     ImGui_ImplDX9_InvalidateDeviceObjects();
     return hooks->reset(device, params);
 }
