68ea76da6f494c561d32088eb709aa5617d276fd
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-13 19:37:17 +0100

    Move implementation of to_json() and from_json() functions to ConfigStructs.h

--- GOESP/Config.cpp
+++ GOESP/Config.cpp
@@ -85,98 +85,9 @@ Config::Config(const char* folderName) noexcept
     std::sort(std::next(systemFonts.begin()), systemFonts.end());
 }
 
-using json = nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int64_t, std::uint64_t, float>;
-using value_t = json::value_t;
-
-template <value_t Type, typename T>
-static void read(const json& j, const char* key, T& o) noexcept
-{
-    if (!j.contains(key))
-        return;
-
-    if (const auto& val = j[key]; val.type() == Type)
-        val.get_to(o);
-}
-
-static void read(const json& j, const char* key, bool& o) noexcept
-{
-    if (!j.contains(key))
-        return;
-
-    if (const auto& val = j[key]; val.type() == value_t::boolean)
-        val.get_to(o);
-}
-
-template <typename T, size_t Size>
-static void read(const json& j, const char* key, std::array<T, Size>& o) noexcept
-{
-    if (!j.contains(key))
-        return;
-
-    if (const auto& val = j[key]; val.type() == value_t::array && val.size() == o.size())
-        val.get_to(o);
-}
-
-template <typename T>
-static void read_number(const json& j, const char* key, T& o) noexcept
-{
-    if (!j.contains(key))
-        return;
-
-    if (const auto& val = j[key]; val.is_number())
-        val.get_to(o);
-}
-
-template <typename T>
-static void read_map(const json& j, const char* key, std::unordered_map<std::string, T>& o) noexcept
-{
-    if (!j.contains(key))
-        return;
-
-    if (const auto& val = j[key]; val.is_object()) {
-        for (auto& element : val.items())
-            element.value().get_to(o[element.key()]);
-    }
-}
-
-static void from_json(const json& j, Color& c)
-{
-    read(j, "Color", c.color);
-    read(j, "Rainbow", c.rainbow);
-    read_number(j, "Rainbow Speed", c.rainbowSpeed);
-}
-
-static void from_json(const json& j, ColorToggle& ct)
-{
-    from_json(j, static_cast<Color&>(ct));
-
-    read(j, "Enabled", ct.enabled);
-}
-
-static void from_json(const json& j, ColorToggleRounding& ctr)
-{
-    from_json(j, static_cast<ColorToggle&>(ctr));
-
-    read_number(j, "Rounding", ctr.rounding);
-}
-
-static void from_json(const json& j, ColorToggleThickness& ctt)
-{
-    from_json(j, static_cast<ColorToggle&>(ctt));
-
-    read_number(j, "Thickness", ctt.thickness);
-}
-
-static void from_json(const json& j, ColorToggleThicknessRounding& cttr)
-{
-    from_json(j, static_cast<ColorToggleRounding&>(cttr));
-
-    read_number(j, "Thickness", cttr.thickness);
-}
-
 static void from_json(const json& j, Font& f)
 {
-    read<value_t::string>(j, "Name", f.name); 
+    read<value_t::string>(j, "Name", f.name);
 
     if (const auto it = std::find_if(config->getSystemFonts().begin(), config->getSystemFonts().end(), [&f](const auto& e) { return e == f.name; }); it != config->getSystemFonts().end()) {
         f.index = std::distance(config->getSystemFonts().begin(), it);
@@ -184,22 +95,6 @@ static void from_json(const json& j, Font& f)
     } else {
         f.index = 0;
     }
-}
-
-static void from_json(const json& j, Snapline& s)
-{
-    from_json(j, static_cast<ColorToggleThickness&>(s));
-
-    read_number(j, "Type", s.type);
-}
-
-static void from_json(const json& j, Box& b)
-{
-    from_json(j, static_cast<ColorToggleRounding&>(b));
-
-    read_number(j, "Type", b.type);
-    read(j, "Scale", b.scale);
-    read<value_t::object>(j, "Fill", b.fill);
 }
 
 static void from_json(const json& j, Shared& s)
@@ -212,28 +107,6 @@ static void from_json(const json& j, Shared& s)
     read_number(j, "Text Cull Distance", s.textCullDistance);
 }
 
-static void from_json(const json& j, Weapon& w)
-{
-    from_json(j, static_cast<Shared&>(w));
-
-    read<value_t::object>(j, "Ammo", w.ammo);
-}
-
-static void from_json(const json& j, Trail& t)
-{
-    from_json(j, static_cast<ColorToggleThickness&>(t));
-
-    read_number(j, "Type", t.type);
-    read_number(j, "Time", t.time);
-}
-
-static void from_json(const json& j, Trails& t)
-{
-    read(j, "Enabled", t.enabled);
-    read<value_t::object>(j, "Local Player", t.localPlayer);
-    read<value_t::object>(j, "Allies", t.allies);
-    read<value_t::object>(j, "Enemies", t.enemies);
-}
 
 static void from_json(const json& j, Projectile& p)
 {
@@ -255,40 +128,11 @@ static void from_json(const json& j, Player& p)
     read(j, "Health Bar", p.healthBar);
 }
 
-static void from_json(const json& j, ImVec2& v)
+static void from_json(const json& j, Weapon& w)
 {
-    read_number(j, "X", v.x);
-    read_number(j, "Y", v.y);
-}
+    from_json(j, static_cast<Shared&>(w));
 
-static void from_json(const json& j, PurchaseList& pl)
-{
-    read(j, "Enabled", pl.enabled);
-    read(j, "Only During Freeze Time", pl.onlyDuringFreezeTime);
-    read(j, "Show Prices", pl.showPrices);
-    read(j, "No Title Bar", pl.noTitleBar);
-    read_number(j, "Mode", pl.mode);
-    read<value_t::object>(j, "Pos", pl.pos);
-    read<value_t::object>(j, "Size", pl.size);
-}
-
-static void from_json(const json& j, ObserverList& ol)
-{
-    read(j, "Enabled", ol.enabled);
-    read(j, "No Title Bar", ol.noTitleBar);
-    read<value_t::object>(j, "Pos", ol.pos);
-    read<value_t::object>(j, "Size", ol.size);
-}
-
-static void from_json(const json& j, OverlayWindow& o)
-{
-    read(j, "Enabled", o.enabled);
-    read<value_t::object>(j, "Pos", o.pos);
-}
-
-static void from_json(const json& j, OffscreenEnemies& o)
-{
-    read(j, "Enabled", o.enabled);
+    read<value_t::object>(j, "Ammo", w.ammo);
 }
 
 void Config::load() noexcept
@@ -315,157 +159,6 @@ void Config::load() noexcept
     read(j, "Ignore Flashbang", ignoreFlashbang);
     read<value_t::object>(j, "FPS Counter", fpsCounter);
     read<value_t::object>(j, "Offscreen Enemies", offscreenEnemies);
-}
-
-// WRITE macro requires:
-// - json object named 'j'
-// - object holding default values named 'dummy'
-// - object to write to json named 'o'
-#define WRITE(name, valueName) \
-if (!(o.valueName == dummy.valueName)) \
-    j[name] = o.valueName;
-
-static void to_json(json& j, const Color& o, const Color& dummy = {})
-{
-    WRITE("Color", color)
-    WRITE("Rainbow", rainbow)
-    WRITE("Rainbow Speed", rainbowSpeed)
-}
-
-static void to_json(json& j, const ColorToggle& o, const ColorToggle& dummy = {})
-{
-    to_json(j, static_cast<const Color&>(o), dummy);
-    WRITE("Enabled", enabled)
-}
-
-static void to_json(json& j, const ColorToggleRounding& o, const ColorToggleRounding& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggle&>(o), dummy);
-    WRITE("Rounding", rounding)
-}
-
-static void to_json(json& j, const ColorToggleThickness& o, const ColorToggleThickness& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggle&>(o), dummy);
-    WRITE("Thickness", thickness)
-}
-
-static void to_json(json& j, const ColorToggleThicknessRounding& o, const ColorToggleThicknessRounding& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggleRounding&>(o), dummy);
-    WRITE("Thickness", thickness)
-}
-
-static void to_json(json& j, const Font& o, const Font& dummy = {})
-{
-    WRITE("Name", name)
-}
-
-static void to_json(json& j, const Snapline& o, const Snapline& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggleThickness&>(o), dummy);
-    WRITE("Type", type)
-}
-
-static void to_json(json& j, const Box& o, const Box& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggleRounding&>(o), dummy);
-    WRITE("Type", type)
-    WRITE("Scale", scale)
-    to_json(j["Fill"], o.fill, dummy.fill);
-}
-
-static void to_json(json& j, const Shared& o, const Shared& dummy = {})
-{
-    WRITE("Enabled", enabled)
-    to_json(j["Font"], o.font, dummy.font);
-    to_json(j["Snapline"], o.snapline, dummy.snapline);
-    to_json(j["Box"], o.box, dummy.box);
-    to_json(j["Name"], o.name, dummy.name);
-    WRITE("Text Cull Distance", textCullDistance)
-}
-
-static void to_json(json& j, const Player& o, const Player& dummy = {})
-{
-    to_json(j, static_cast<const Shared&>(o), dummy);
-    to_json(j["Weapon"], o.weapon, dummy.weapon);
-    to_json(j["Flash Duration"], o.flashDuration, dummy.flashDuration);
-    WRITE("Audible Only", audibleOnly)
-    WRITE("Spotted Only", spottedOnly)
-    to_json(j["Skeleton"], o.skeleton, dummy.skeleton);
-    to_json(j["Head Box"], o.headBox, dummy.headBox);
-    WRITE("Health Bar", healthBar)
-}
-
-static void to_json(json& j, const Weapon& o, const Weapon& dummy = {})
-{
-    to_json(j, static_cast<const Shared&>(o), dummy);
-    to_json(j["Ammo"], o.ammo, dummy.ammo);
-}
-
-static void to_json(json& j, const Trail& o, const Trail& dummy = {})
-{
-    to_json(j, static_cast<const ColorToggleThickness&>(o), dummy);
-    WRITE("Type", type)
-    WRITE("Time", time)
-}
-
-static void to_json(json& j, const Trails& o, const Trails& dummy = {})
-{
-    WRITE("Enabled", enabled)
-    to_json(j["Local Player"], o.localPlayer, dummy.localPlayer);
-    to_json(j["Allies"], o.allies, dummy.allies);
-    to_json(j["Enemies"], o.enemies, dummy.enemies);
-}
-
-static void to_json(json& j, const Projectile& o, const Projectile& dummy = {})
-{
-    to_json(j, static_cast<const Shared&>(o), dummy);
-    to_json(j["Trails"], o.trails, dummy.trails);
-}
-
-static void to_json(json& j, const ImVec2& o, const ImVec2& dummy = {})
-{
-    WRITE("X", x)
-    WRITE("Y", y)
-}
-
-static void to_json(json& j, const PurchaseList& o, const PurchaseList& dummy = {})
-{
-    WRITE("Enabled", enabled)
-    WRITE("Only During Freeze Time", onlyDuringFreezeTime)
-    WRITE("Show Prices", showPrices)
-    WRITE("No Title Bar", noTitleBar)
-    WRITE("Mode", mode)
-
-    if (const auto window = ImGui::FindWindowByName("Purchases")) {
-        j["Pos"] = window->Pos;
-        j["Size"] = window->SizeFull;
-    }
-}
-
-static void to_json(json& j, const ObserverList& o, const ObserverList& dummy = {})
-{
-    WRITE("Enabled", enabled)
-    WRITE("No Title Bar", noTitleBar)
-
-    if (const auto window = ImGui::FindWindowByName("Observer List")) {
-        j["Pos"] = window->Pos;
-        j["Size"] = window->SizeFull;
-    }
-}
-
-static void to_json(json& j, const OverlayWindow& o, const OverlayWindow& dummy = {})
-{
-    WRITE("Enabled", enabled)
-
-    if (const auto window = ImGui::FindWindowByName(o.name))
-        j["Pos"] = window->Pos;
-}
-
-static void to_json(json& j, const OffscreenEnemies& o, const OffscreenEnemies& dummy = {})
-{
-    WRITE("Enabled", enabled)
 }
 
 void removeEmptyObjects(json& j) noexcept
