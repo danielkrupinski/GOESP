e23c1049148962b94b900720d07a87374555669e
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-13 19:22:13 +0100

    Remove worldToScreen() function from GameData.cpp

--- GOESP/GameData.cpp
+++ GOESP/GameData.cpp
@@ -50,20 +50,6 @@ static std::vector<LootCrateData> lootCrateData;
 static std::list<ProjectileData> projectileData;
 static std::vector<BombData> bombData;
 
-static bool worldToScreen(const Vector& in, ImVec2& out) noexcept
-{
-    const auto& matrix = viewMatrix;
-
-    const auto w = matrix._41 * in.x + matrix._42 * in.y + matrix._43 * in.z + matrix._44;
-    if (w < 0.001f)
-        return false;
-
-    out = ImGui::GetIO().DisplaySize / 2.0f;
-    out.x *= 1.0f + (matrix._11 * in.x + matrix._12 * in.y + matrix._13 * in.z + matrix._14) / w;
-    out.y *= 1.0f - (matrix._21 * in.x + matrix._22 * in.y + matrix._23 * in.z + matrix._24) / w;
-    return true;
-}
-
 void GameData::update() noexcept
 {
     static int lastFrame;
