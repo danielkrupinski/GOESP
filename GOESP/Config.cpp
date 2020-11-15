7acfaa8586c5cb6c76369709399bb68533d58733
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-15 09:42:49 +0100

    Remove fontCallback() function from Config.cpp

--- GOESP/Config.cpp
+++ GOESP/Config.cpp
@@ -16,39 +16,6 @@
 #include "Hacks/ESP.h"
 #include "Hacks/Misc.h"
 
-#ifdef _WIN32
-int CALLBACK fontCallback(const LOGFONTW* lpelfe, const TEXTMETRICW*, DWORD, LPARAM lParam)
-{
-    const wchar_t* const fontName = reinterpret_cast<const ENUMLOGFONTEXW*>(lpelfe)->elfFullName;
-
-    if (fontName[0] == L'@')
-        return TRUE;
-
-    if (HFONT font = CreateFontW(0, 0, 0, 0,
-        FW_NORMAL, FALSE, FALSE, FALSE,
-        ANSI_CHARSET, OUT_DEFAULT_PRECIS,
-        CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
-        DEFAULT_PITCH, fontName)) {
-
-        DWORD fontData = GDI_ERROR;
-
-        if (HDC hdc = CreateCompatibleDC(nullptr)) {
-            SelectObject(hdc, font);
-            // Do not use TTC fonts as we only support TTF fonts
-            fontData = GetFontData(hdc, 'fctt', 0, NULL, 0);
-            DeleteDC(hdc);
-        }
-        DeleteObject(font);
-
-        if (fontData == GDI_ERROR) {
-            if (char buff[1024]; WideCharToMultiByte(CP_UTF8, 0, fontName, -1, buff, sizeof(buff), nullptr, nullptr))
-                reinterpret_cast<std::vector<std::string>*>(lParam)->emplace_back(buff);
-        }
-    }
-    return TRUE;
-}
-#endif
-
 Config::Config() noexcept
 {
 
