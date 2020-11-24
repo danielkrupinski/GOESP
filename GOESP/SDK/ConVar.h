134ddedabcbb144855322ea76589cb913f5f5c24
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-24 19:48:30 +0100

    Fix ConVar::getFloat() having incorrect index on linux

--- GOESP/SDK/ConVar.h
+++ GOESP/SDK/ConVar.h
@@ -3,5 +3,9 @@
 #include "VirtualMethod.h"
 
 struct ConVar {
+#ifdef _WIN32
     VIRTUAL_METHOD(float, getFloat, 12, (), (this))
+#else
+    VIRTUAL_METHOD(float, getFloat, 15, (), (this))
+#endif
 };
