d92bd2714b14960dc84c25bedbe0cf2cfd32c810
Daniel Krupiński <danielkrupinski@outlook.com>
2020-12-15 14:46:40 +0100

    Add skillgroup1 texture

--- GOESP/Resources/skillgroups.h
+++ GOESP/Resources/skillgroups.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "../Helpers.h"
+
+// File: 'skillgroup1.png' (1126 bytes)
+// Exported using binary_to_compressed_c.cpp
+
+inline constexpr auto skillgroup1 = Helpers::decodeBase85(
+"=pxq9_8v7&0C/2'K(HH=T3Xc27-CG).,###))Qs.%.>>#@-s*>16ds8i.')Sx44,R$i,aS8RmaSucr.Qa5%JQZAUXV6M+,Rq1kcR]@4LZ$cA4Y,vWcR0[xN5a?xIQ[gc(56guMYT$*BT"
+"/(Q&TAPq37>(61<Fv.q9Y;L&I5#3,R6$A?U#5GSYj=>/fA`oOY(AfjPJxP<Lud`T9`C(S:VXZL<[@$1<I/NX8F(hk;lWS+>VloU9^aXc>`.Uk;:cPd=ZS8q9l&Gc>nh:8:e#()?E-HW8"
+"H1TZAZba`5o4n]@Z?7qCtpl>7<E7qCM'RmDd(2f3h0X>72u9i`U7NL2YBf],D*jBTSP0?Kn;WT/+1^SX7jvLPCF<RNqueYmXFTXk0?('iHQ1a_S,kG]/C1MZ=8.xLg$#,HGIFld^m55O"
+"st/JGC@eMYa####a2]1<2Tm,]?&,E^Iv3K[RA,E^3#trs91?0[m)rbs*Rq&j_*&$U%Z,k+*#2Z4=7YY#_=$w7&^GT1<1pD.a]vxkeQ+jmLfhsr.wUZ_rr;]9nBm3vno:iQo&Oe,Q_$St"
+"=a*Ta$m2#K:,hdhh>OmUujZ>5%_-ara(,oXxO(dtVf]$'b8VF)8&:3==ttmD5;6N+hZkWsZ#k2l44MJ*qv:X=cv&+(dK_2$:$H=o:elRf$.lxgF),OJ.ongNwGfBXBLVK&l7YI-+'3vQ"
+"@``'d.,M5*/4ljAA;V_chB,uNoxw`O)B.[GAXop]$1VJ1`.xMn?`U=FJ%B1MKOfs*[GN3Nc?`C?^C(^],%+&T@$esr.PcOiQ[rrO_/Uh3=D@Dt4N+r3CFO3RV$BBKNjQR7^$N&c:8WvP"
+"Do1]$^)9oW&X5?9OODQX*<GXKm'.uG5Z3J&XZ=L&,[GUYZZbX0C_Z$1jIOXTv/$9Y__LRI'oA=&f@Cd#vXZRQ8AHvY8GckB>d2]gssQ<8-7LbFYI[/ZYc6O@MUYPbs14o,XHvl2@M%K;"
+"a@..4@]D*Wvw27vsSKr^7F/jHS+TM^>$v3)Gg#I$MVG.ixF?Nn41`vrnr0JIx*V9%%+m?L'SA='i%cWOQT`M6RvJk?gR.q@,h7UDlqlu$qB-A8+p#_2dJGAl@PQxpOj_Wa)uw#],Px]k"
+"v1cQ(ejKhU]6n$,l[4m8WK1r^-TTScf'B^Z>;eteT`Dx5Dhe%cJdg,O[';NueR4Xf2;f3I>e*xAKc%I>ssem4Dwkr'x&?b#8S[wQJctssNU:Q[1M]MeDPR->q&P=EaQ]%+&]6OP/XYUJ"
+"wB71n+'[]D7dj2BHpPce#fRwS.U?MrN$Pv`,:#v%x9ihU8m2R-J%q$QMDPL2TFG)FF90bO/_9bSHs5kf-E5En_,V;qk)u9G7eig_ns<4MAl_SlSKH3ow-:SaBC`BS+AAfri,)##]Kg99"
+"QaYG8[W'##");
