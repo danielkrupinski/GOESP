8c0d4b9293d2eab0b400575bce74084f6473a4a9
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-25 13:46:03 +0100

    Deprecate GameData::toScreenMatrix() function

--- GOESP/GameData.h
+++ GOESP/GameData.h
@@ -38,7 +38,7 @@ namespace GameData
     };
 
     // You have to acquire lock before using these getters
-    const Matrix4x4& toScreenMatrix() noexcept;
+    [[deprecated]] const Matrix4x4& toScreenMatrix() noexcept;
     bool worldToScreen(const Vector& in, ImVec2& out, bool floor = false) noexcept;
     const LocalPlayerData& local() noexcept;
     const std::vector<PlayerData>& players() noexcept;
