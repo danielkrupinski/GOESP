3ae5fd10bff927a1c707677731b2dc1bd0404998
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-28 10:01:20 +0100

    Clean up InputSystem class

--- GOESP/SDK/InputSystem.h
+++ GOESP/SDK/InputSystem.h
@@ -8,42 +8,4 @@ public:
     VIRTUAL_METHOD(void, enableInput, 11, (bool enable), (this, enable))
 #endif
     VIRTUAL_METHOD(void, resetInputState, 39, (), (this))
-    VIRTUAL_METHOD(const char*, buttonCodeToString, 40, (int buttonCode), (this, buttonCode))
-    VIRTUAL_METHOD(int, stringToButtonCode, 42, (const char* keyName), (this, keyName))
-
-    // does not support mouse buttons
-    VIRTUAL_METHOD(int, virtualKeyToButtonCode, 45, (int virtualKey), (this, virtualKey))
-    VIRTUAL_METHOD(int, buttonCodeToVirtualKey, 46, (int buttonCode), (this, buttonCode))
-
-    auto keyToButtonCode(int key) noexcept
-    {
-        if (key < 512)
-            return virtualKeyToButtonCode(key);
-
-        if (key >= 512 && key <= 518)
-            return key - 405;
-
-        return 0;
-    }
-
-    auto buttonCodeToKey(int buttonCode) noexcept
-    {
-        if (buttonCode < 107)
-            return buttonCodeToVirtualKey(buttonCode);
-
-        if (buttonCode >= 107 && buttonCode <= 113)
-            return buttonCode + 405;
-
-        return 0;
-    }
-
-    auto keyToString(int key) noexcept
-    {
-        return buttonCodeToString(keyToButtonCode(key));
-    }
-
-    auto stringToKey(const char* keyName) noexcept
-    {
-        return buttonCodeToKey(stringToButtonCode(keyName));
-    }
 };
