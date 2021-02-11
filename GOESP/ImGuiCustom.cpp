3ef4d5a562ff67f59236f4d2f1ef0907c7aec560
Daniel Krupiński <danielkrupinski@outlook.com>
2021-02-11 15:18:50 +0100

    Greatly improve progress bar (such as in "Bomb Timer") smoothness

--- GOESP/ImGuiCustom.cpp
+++ GOESP/ImGuiCustom.cpp
@@ -122,7 +122,21 @@ void ImGui::progressBarFullWidth(float fraction, float height) noexcept
     fraction = ImSaturate(fraction);
     RenderFrame(bb.Min, bb.Max, GetColorU32(ImGuiCol_FrameBg), true, style.FrameRounding);
     bb.Expand(ImVec2(-style.FrameBorderSize, -style.FrameBorderSize));
-    RenderRectFilledRangeH(window->DrawList, bb, GetColorU32(ImGuiCol_PlotHistogram), 0.0f, fraction, style.FrameRounding);
+
+    if (fraction == 0.0f)
+        return;
+
+    const ImVec2 p0{ ImLerp(bb.Min.x, bb.Max.x, 0.0f), bb.Min.y };
+    const ImVec2 p1{ ImLerp(bb.Min.x, bb.Max.x, fraction), bb.Max.y };
+
+    const float x0 = ImMax(p0.x, bb.Min.x);
+    const float x1 = ImMin(p1.x, bb.Max.x);
+
+    window->DrawList->PathLineTo({ x0, p1.y });
+    window->DrawList->PathLineTo({ x0, p0.y });
+    window->DrawList->PathLineTo({ x1, p0.y });
+    window->DrawList->PathLineTo({ x1, p1.y });
+    window->DrawList->PathFillConvex(GetColorU32(ImGuiCol_PlotHistogram));
 }
 
 bool ImGui::beginTable(const char* str_id, int columns_count, ImGuiTableFlags flags, const ImVec2& outer_size, float inner_width) noexcept
