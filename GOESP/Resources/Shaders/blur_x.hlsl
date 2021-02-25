951608c437a311e262a23937289a38c14ca4072f
Daniel Krupiński <danielkrupinski@outlook.com>
2021-02-25 13:00:03 +0100

    Add blur_x.hlsl shader

---
+++ GOESP/Resources/Shaders/blur_x.hlsl
@@ -0,0 +1,19 @@
+sampler texSampler;
+
+float texelWidth;
+
+static const float offsets[3] = { 0.0, 1.3846153846, 3.2307692308 };
+static const float weights[3] = { 0.2270270270, 0.3162162162, 0.0702702703 };
+
+float4 main(float2 uv : TEXCOORD0) : COLOR0
+{
+    float4 color = tex2D(texSampler, uv);
+    (float3)color *= weights[0];
+
+    for (int i = 1; i < 3; ++i) {
+        (float3)color += (float3)tex2D(texSampler, uv - float2(texelWidth * offsets[i], 0.0f)) * weights[i];
+        (float3)color += (float3)tex2D(texSampler, uv + float2(texelWidth * offsets[i], 0.0f)) * weights[i];
+    }
+
+    return color;
+}
