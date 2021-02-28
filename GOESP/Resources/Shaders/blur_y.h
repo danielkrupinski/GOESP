590ece1536a128c044a9c258587ea48f79689c23
Daniel Krupiński <danielkrupinski@outlook.com>
2021-02-28 16:54:27 +0100

    Update blur pixel shaders to work correctly on a downsampled texture

--- GOESP/Resources/Shaders/blur_y.h
+++ GOESP/Resources/Shaders/blur_y.h
@@ -5,14 +5,17 @@
 namespace Resource
 {
 
-// File: 'blur_y' (656 bytes)
+// File: 'blur_y' (968 bytes)
 // Exported using binary_to_compressed_c.cpp
 inline constexpr auto blur_y = decodeBase85(
 "%/P:vP>$(#>T$<8?####Q$###%/P:v%####?####$&###J$###h####&####$####t#########.$###%####$####:$########tn=j=$8HlEQ2TuY3l:$#%),##$#########0g'WC"
-"Qk;nDhpF/#&5>##%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHVd;#K7NSi.LX/'.7`7r'7$QUV$*%XHV"
-"d:i[7bmhf6##########D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V&5YY#W.]/Lpu$GV+>uu#PYr.LOV%JLou$GV&5YY#Q`%/LP].JL&5YY#PYr.Lpv*PV(>uu#"
-"Rf./L4_/GLdfIl[&5YY#QYr.L)[-S[+G:;$R`%/Lou$GVOV%JL)]3][&5YY#Y.]/Lqu$GV+>uu#Sl7/LQV%JLou$GV&5YY#S`%/LP_:]L&5YY#RYr.Lpv*PV(>uu#Tf./L6_/GLdfIl["
-"&5YY#SYr.L)[-S[+G:;$T`%/Lou$GVQV%JL)]3][i>uu#_XF0L4_/GL[BA`Vi>uu#`XF0L5_/GL[BA`Vi>uu#aXF0L6_/GL[BA`Vi>uu#bXF0L7_/GL[BA`V+>uu#V(S/L4_/GLpw0YV"
-"+G:;$V(S/L3_/GLpx6cV4_/GL+G:;$V(S/L5_/GLpw0YV4_/GL+G:;$V(S/L6_/GLqv*PV4_/GL+G:;$U(S/L7_/GLqv*PV4_/GL&5YY#fqF0L3_/GL#),##");
+"Qk;nDhpF/#&5>##%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHVdfES7N=7Z7DaFa69k(b7$QUV$*%XHV"
+"NSi.LfxB$7`7r'71i%r6$QUV$+%XHVF@=I6###############D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V&5YY#PYr.Lqu$GV&5YY#Q`%/Lp(@cV(>uu#Rf./L"
+"4_/GLdfIl[&5YY#QYr.L)[-S[(>uu#R`%/L)]3][ou$GV(>uu#Sl7/Lou$GVou$GV&5YY#S`%/LP_:]L&5YY#RYr.Lqu$GV(>uu#Tf./L6_/GLdfIl[&5YY#SYr.L)[-S[&5YY#_@x/L"
+"T*A`V+G:;$T`%/Lou$GVSSi.L)]3][+>uu#Sl7/LSTo7Lou$GV&5YY#V`%/LP_:]L&5YY#UYr.Lqu$GV(>uu#Wf./L9_/GLdfIl[&5YY#VYr.L)[-S[+G:;$W`%/Lou$GVSTo7L)]3]["
+"+>uu#Sl7/LSV%JLou$GV&5YY#U`%/LP_:]L&5YY#TYr.Lqu$GV(>uu#Vf./L8_/GLdfIl[&5YY#WYr.L)[-S[+G:;$X`%/Lou$GVSV%JL)]3][i>uu#_XF0L4_/GL[BA`Vi>uu#`XF0L"
+"5_/GL[BA`Vi>uu#aXF0L6_/GL[BA`Vi>uu#bXF0L7_/GL[BA`Vi>uu#dXF0L9_/GL[BA`Vi>uu#eXF0L:_/GL[BA`Vi>uu#cXF0L8_/GL[BA`Vi>uu#fXF0L;_/GL[BA`V+>uu#V(S/L"
+"4_/GLqv*PV+G:;$V(S/L3_/GLqw0YV4_/GL+G:;$V(S/L5_/GLqv*PV4_/GL+G:;$V(S/L6_/GLqx6cV4_/GL+G:;$V(S/L7_/GLqx6cV4_/GL+G:;$V(S/L9_/GLpw0YV4_/GL+G:;$"
+"V(S/L:_/GLpw0YV4_/GL+G:;$V(S/L8_/GLru$GV4_/GL+G:;$U(S/L;_/GLru$GV4_/GL&5YY#fqF0L3_/GL#),##");
 
 }
--- GOESP/Resources/Shaders/blur_y.hlsl
+++ GOESP/Resources/Shaders/blur_y.hlsl
@@ -2,15 +2,15 @@ sampler texSampler;
 
 float texelHeight;
 
-static const float offsets[3] = { 0.0, 1.3846153846, 3.2307692308 };
-static const float weights[3] = { 0.2270270270, 0.3162162162, 0.0702702703 };
+static const float offsets[5] = { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f };
+static const float weights[5] = { 0.2270270270f, 0.1945945946f, 0.1216216216f, 0.0540540541f, 0.0162162162f };
 
 float4 main(float2 uv : TEXCOORD0) : COLOR0
 {
     float4 color = tex2D(texSampler, uv);
     (float3)color *= weights[0];
 
-    for (int i = 1; i < 3; ++i) {
+    for (int i = 1; i < 5; ++i) {
         (float3)color += (float3)tex2D(texSampler, uv - float2(0.0f, texelHeight * offsets[i])) * weights[i];
         (float3)color += (float3)tex2D(texSampler, uv + float2(0.0f, texelHeight * offsets[i])) * weights[i];
     }
