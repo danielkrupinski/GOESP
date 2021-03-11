7b32228c6d794c8db69e7adbdcdfd6eaaf4411b1
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-11 10:43:58 +0100

    Add Shaders/monochrome.h

---
+++ GOESP/Resources/Shaders/monochrome.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Resource.h"
+
+namespace Resource
+{
+
+// File: 'monochrome' (240 bytes)
+// Exported using binary_to_compressed_c.cpp
+inline constexpr auto monochrome = decodeBase85(
+"%/P:vDKb&#>T$<8?####w####%/P:v$####?####$&###p####S####&####$####`#########tn=j=$8HlEQ2TuY3l:$#%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe="
+"K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$(%XHV?7@&7`Dv=7k`6g6#####D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V.>uu#U(S/L3_/GLS*A`V"
+"&5YY#fqF0L3_/GL#),##");
+
+}
