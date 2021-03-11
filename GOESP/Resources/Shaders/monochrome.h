a37c6b87e281466cee4356fb7d6c6f9b164a6e50
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-11 13:45:06 +0100

    Add monochrome shader argument controlling monochrome amount

--- GOESP/Resources/Shaders/monochrome.h
+++ GOESP/Resources/Shaders/monochrome.h
@@ -5,11 +5,11 @@
 namespace Resource
 {
 
-// File: 'monochrome' (240 bytes)
+// File: 'monochrome' (316 bytes)
 // Exported using binary_to_compressed_c.cpp
 inline constexpr auto monochrome = decodeBase85(
-"%/P:vDKb&#>T$<8?####w####%/P:v$####?####$&###p####S####&####$####`#########tn=j=$8HlEQ2TuY3l:$#%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe="
-"K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$(%XHV?7@&7`Dv=7k`6g6#####D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V.>uu#U(S/L3_/GLS*A`V"
-"&5YY#fqF0L3_/GL#),##");
+"%/P:vO8q'#>T$<8?####M$###%/P:v%####?####$&###F$###h####%####$####p#########*$###&####$####6$########,.4cH]8TuY&5>##%),##$#########tn=j=$8HlE"
+"Q2TuY3l:$#%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHV?7@&7`Dv=7k`6g6#####D5YY#NSi.L,nHS["
+"D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V.>uu#W.]/L3_/GLT*A`V9G:;$W(S/Lou$GVOV%JL3_/GL&5YY#X.]/LNV%JL&5YY#fqF0L5_/GL#),##");
 
 }
