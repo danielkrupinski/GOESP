63782ca0aa5f3d7958d0c416d4fb84f2665cd899
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-16 13:10:44 +0100

    Benefit from linear sampling in vertical blur pixel shader

--- GOESP/Resources/Shaders/blur_y.h
+++ GOESP/Resources/Shaders/blur_y.h
@@ -5,16 +5,14 @@
 namespace Resource
 {
 
-// File: 'blur_y' (896 bytes)
+// File: 'blur_y' (608 bytes)
 // Exported using binary_to_compressed_c.cpp
 inline constexpr auto blur_y = decodeBase85(
-"%/P:vP>$(#>T$<8?####Q$###%/P:v%####?####$&###J$###h####&####$####t#########.$###%####$####:$########tn=j=$8HlEQ2TuY3l:$#%),##$#########0g'WC"
-"Qk;nDhpF/#&5>##%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHV$o'gaDaFa6N=7Z7eEoma$QUV$*%XHV"
-"fxB$7`7r'79A6`a1i%r6$QUV$+%XHVF@=I6###############D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V&5YY#PYr.L)[-S[(>uu#Q`%/L)]3][ou$GV&5YY#"
-"QYr.L)[-S[(>uu#R`%/L)]3][p(@cV&5YY#RYr.L)[-S[&5YY#Sl7/Lqw0YV+G:;$S`%/Lou$GVOUu@L)]3][&5YY#SYr.L)[-S[+G:;$T`%/Lou$GVP_:]L)]3][&5YY#TYr.L)[-S["
-"&5YY#bL40LT*A`V+G:;$U`%/Lou$GVTSi.L)]3][&5YY#UYr.L)[-S[+G:;$V`%/Lou$GVTUu@L)]3][&5YY#VYr.L)[-S[+G:;$W`%/Lou$GVTV%JL)]3][&5YY#WYr.L)[-S[+G:;$"
-"X`%/Lou$GVU`@fL)]3][i>uu#_XF0L4_/GL[BA`Vi>uu#`XF0L5_/GL[BA`Vi>uu#aXF0L6_/GL[BA`Vi>uu#bXF0L7_/GL[BA`Vi>uu#cXF0L8_/GL[BA`Vi>uu#dXF0L9_/GL[BA`V"
-"i>uu#eXF0L:_/GL[BA`Vi>uu#fXF0L;_/GL[BA`V+>uu#W(S/L5_/GLqu$GV+G:;$W(S/L3_/GLqv*PV5_/GL+G:;$V(S/L4_/GLqu$GV5_/GL+G:;$V(S/L6_/GLqx6cV4_/GL+G:;$"
-"V(S/L7_/GLqx6cV4_/GL+G:;$V(S/L8_/GLpv*PV4_/GL+G:;$V(S/L9_/GLpv*PV4_/GL+G:;$V(S/L:_/GLru$GV4_/GL+G:;$U(S/L;_/GLru$GV4_/GL&5YY#fqF0L3_/GL#),##");
+"%/P:vP>$(#>T$<8?####Q$###%/P:v%####?####OS'##J$###h####&####$####t#########.$###%####$####:$########tn=j=$8HlEQ2TuY3l:$#%),##$#########0g'WC"
+"Qk;nDhpF/#&5>##%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHV9miVaX/'.7`7r'7d;#K7$QUV$*%XHV"
+"9lXhabmhf6d:i[7#####D5YY#NSi.L,nHS[D5YY#_9r:Q0=XHVi>uu#^XF0LdfIl[[BA`V&5YY#PYr.L)[-S[&5YY#Y4f/LT*A`V+G:;$Q`%/Lou$GVPV%JL)]3][&5YY#RYr.L)[-S["
+"+G:;$S`%/Lou$GVPSi.L)]3][&5YY#QYr.L)[-S[&5YY#Wr@/LU*A`V+G:;$R`%/Lou$GVRSi.L)]3][&5YY#SYr.L)[-S[+G:;$T`%/Lou$GVRUu@L)]3][i>uu#_XF0L4_/GL[BA`V"
+"i>uu#aXF0L6_/GL[BA`Vi>uu#`XF0L5_/GL[BA`Vi>uu#bXF0L7_/GL[BA`V+>uu#X(S/L6_/GLpv*PV+G:;$X(S/L3_/GLpw0YV6_/GL+G:;$V(S/L4_/GLpv*PV6_/GL+G:;$V(S/L"
+"5_/GLqv*PV4_/GL+G:;$U(S/L7_/GLqv*PV4_/GL&5YY#fqF0L3_/GL#),##");
 
 }
