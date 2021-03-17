bf1604734738ad410e85fc0d731a63f2a84f1a4f
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-17 19:21:29 +0100

    Specify inputs locations in GLSL vertex blur shader

--- GOESP/Resources/Shaders/blur.glsl
+++ GOESP/Resources/Shaders/blur.glsl
@@ -2,8 +2,8 @@ R"(
 #version 130
 #extension GL_ARB_explicit_attrib_location : require
 
-in vec2 pos;
-in vec2 uv;
+layout(location = 0) in vec2 pos;
+layout(location = 1) in vec2 uv;
 
 out vec2 fragUV;
 
