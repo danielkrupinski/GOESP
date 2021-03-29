ab1a5f16e351d0c2646dad1964a8ff46a52bd33d
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-29 19:10:03 +0200

    Use FreeType font rasterizer instead of stb_truetype

--- GOESP/imgui/imconfig.h
+++ GOESP/imgui/imconfig.h
@@ -16,6 +16,10 @@
 #include <array>
 #include <tuple>
 
+#ifdef _WIN32
+#define IMGUI_ENABLE_FREETYPE
+#endif
+
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
