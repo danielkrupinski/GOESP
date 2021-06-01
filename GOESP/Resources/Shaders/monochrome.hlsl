66508e7d905a734bf5810b3c62767e13ba371542
Daniel Krupiński <danielkrupinski@outlook.com>
2021-06-01 19:34:03 +0200

    Simplify monochrome pixel shader

--- GOESP/Resources/Shaders/monochrome.hlsl
+++ GOESP/Resources/Shaders/monochrome.hlsl
@@ -5,6 +5,6 @@ float amount;
 float4 main(float2 uv : TEXCOORD0) : COLOR0
 {
     float4 color = tex2D(texSampler, uv);
-    float gray = dot(color.rgb, float3(0.2126f, 0.7152f, 0.0722f));
-    return float4(lerp(color.rgb, float3(gray, gray, gray), amount), 1.0f);
+    color.rgb = lerp(color.rgb, dot(color.rgb, float3(0.2126f, 0.7152f, 0.0722f)), amount);
+    return color;
 }
