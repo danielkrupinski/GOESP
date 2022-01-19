b763092aee46c92863e8902e6747514fd8d831de
Daniel Krupiński <danielkrupinski@outlook.com>
2022-01-19 21:37:38 +0100

    Create Tests/MemoryTests.cpp

---
+++ Tests/MemoryTests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "../GOESP/Memory.cpp"
+
+class RelativeToAbsoluteTestFixture : public testing::TestWithParam<std::int32_t> {};
+
+TEST_P(RelativeToAbsoluteTestFixture, CorrectAddressIsComputed) {
+    const std::int32_t offset = GetParam();
+    const std::uintptr_t addressOfOffset = reinterpret_cast<std::uintptr_t>(&offset);
+
+    ASSERT_EQ(relativeToAbsolute<std::uintptr_t>(addressOfOffset), addressOfOffset + offset + sizeof(offset));
+}
+
+INSTANTIATE_TEST_SUITE_P(RelativeToAbsoluteTest, RelativeToAbsoluteTestFixture, testing::Values(-1, 0, 1, 10'000));
