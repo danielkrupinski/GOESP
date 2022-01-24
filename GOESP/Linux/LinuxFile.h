2f16c3bb46e53ad2de9d51e7c3d8ebe899b2fc2a
Daniel Krupiński <danielkrupinski@outlook.com>
2022-01-24 19:52:02 +0100

    Remove LinuxFile::map() method

--- GOESP/Linux/LinuxFile.h
+++ GOESP/Linux/LinuxFile.h
@@ -5,8 +5,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "LinuxFileMap.h"
-
 class LinuxFile {
 public:
     LinuxFile(const char* path, int flags) : descriptor{ open(path, flags) } {}
@@ -26,11 +24,6 @@ public:
         struct stat status;
         fstat(descriptor, &status);
         return status;
-    }
-
-    [[nodiscard]] LinuxFileMap map(void* address, std::size_t length, int protection, int flags, off_t offset) const
-    {
-        return LinuxFileMap{ address, length, protection, flags, descriptor, offset };
     }
 
     ~LinuxFile()
